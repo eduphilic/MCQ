c4c5e252a6756f4277aa00fb5ecc03c02bd35fd3
strothj jason@jasons.io <jason@jasons.io>
2018-04-23 04:45:20 -0400

    feat: restore scroll position on page change

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,3 +1,4 @@
+import { PersistentScrollPositionProvider } from "components/utils/PersistentScrollPosition";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { LightTheme, ThemeBaseline } from "theme";
@@ -14,16 +15,18 @@ const App = () => (
   <ThemeBaseline>
     <LightTheme>
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/resetPassword" component={PasswordReset} />
-          <Route path="/welcome" component={Onboarding} />
+        <PersistentScrollPositionProvider>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/resetPassword" component={PasswordReset} />
+            <Route path="/welcome" component={Onboarding} />
 
-          {/* Start Admin Dashboard Routes */}
-          <Route exact path="/admin" component={AdminLogin} />
-          <Route path="/admin/dashboard" component={AdminDashboard} />
-          <Route path="/admin" component={AdminPlaceholder} />
-        </Switch>
+            {/* Start Admin Dashboard Routes */}
+            <Route exact path="/admin" component={AdminLogin} />
+            <Route path="/admin/dashboard" component={AdminDashboard} />
+            <Route path="/admin" component={AdminPlaceholder} />
+          </Switch>
+        </PersistentScrollPositionProvider>
       </Router>
     </LightTheme>
   </ThemeBaseline>
---
+++ packages/components/utils/PersistentScrollPosition/index.tsx
@@ -0,0 +1,69 @@
+import { Location } from "history";
+import React, { Component, createContext } from "react";
+import { RouteComponentProps, withRouter } from "react-router-dom";
+
+export interface PersistentScrollPositionProps
+  extends RouteComponentProps<any> {}
+
+interface PersistentScrollPositionState {
+  location: Location | null;
+}
+
+const initialState: PersistentScrollPositionState = { location: null };
+const PersistentScrollPositionContext = createContext<
+  PersistentScrollPositionState
+>(initialState);
+
+/**
+ * Stores page scroll positions to session storage so that navigation among
+ * pages start at the top or a previous position.
+ *
+ * Implemented using React.Context so that behavior can be overridden/extended
+ * laster if needed, for instance in tab interfaces.
+ *
+ * @see https://github.com/ReactTraining/react-router/blob/master/packages/react-router-dom/docs/guides/scroll-restoration.md
+ */
+class PersistentScrollPositionProviderBase extends Component<
+  PersistentScrollPositionProps,
+  PersistentScrollPositionState
+> {
+  static getDerivedStateFromProps(
+    nextProps: PersistentScrollPositionProps,
+    prevState: PersistentScrollPositionState,
+  ): Partial<PersistentScrollPositionState> | null {
+    const { location } = nextProps;
+    if (prevState.location === location) return null;
+
+    // Store scroll position of last page.
+    if (prevState.location !== null) {
+      sessionStorage.setItem(
+        prevState.location.pathname,
+        JSON.stringify({ x: window.scrollX, y: window.scrollY }),
+      );
+    }
+
+    // Restore scroll position if available or scroll to top.
+    const previousScrollPosition = sessionStorage.getItem(location.pathname);
+    if (previousScrollPosition) {
+      window.scrollTo(JSON.parse(previousScrollPosition));
+    } else {
+      window.scrollTo(0, 0);
+    }
+
+    return { location };
+  }
+
+  state: PersistentScrollPositionState = { location: null };
+
+  render() {
+    return (
+      <PersistentScrollPositionContext.Provider value={this.state}>
+        {this.props.children}
+      </PersistentScrollPositionContext.Provider>
+    );
+  }
+}
+
+export const PersistentScrollPositionProvider = withRouter(
+  PersistentScrollPositionProviderBase,
+);
