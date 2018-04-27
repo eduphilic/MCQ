665333a24daa0733f67511319d5daade30a88327
strothj jason@jasons.io <jason@jasons.io>
2018-04-27 02:23:48 -0400

    chore: upgrade to babel 7 in storybook

--- packages/components/.storybook/webpack.config.js
+++ packages/components/.storybook/webpack.config.js
@@ -1,50 +1,96 @@
 const path = require("path");
-const webpack = require("webpack");
 const { findMonorepo } = require("react-dev-utils/workspaceUtils");
 
 module.exports = (baseConfig, env, defaultConfig) => {
   const config = defaultConfig;
 
-  // Allow Storybook to transpile packages included from the monorepo.
-  const monorepoPackages = findMonorepo(path.resolve(__dirname, "..")).pkgs;
-  const babelLoader = config.module.rules.find(
-    l => l.loader && l.loader.includes("babel-loader"),
+  // Remove Babel 6 configuration.
+  config.module.rules = config.module.rules.filter(
+    r => !(r.loader && r.loader.includes("babel-loader")),
   );
-  babelLoader.include.push(...monorepoPackages);
 
-  // Add SVG React component support like Create React App.
-  babelLoader.query.plugins.unshift([
-    "babel-plugin-named-asset-import",
-    {
-      loaderMap: {
-        svg: {
-          ReactComponent: "svgr/webpack![path]",
-        },
-      },
-    },
-  ]);
-
-  // Transpile TypeScript.
-  config.module.rules.push({
-    test: /\.tsx?$/,
-    include: babelLoader.include,
-    exclude: babelLoader.exclude,
+  // Add Babel 7 configuration.
+  const monorepoPackages = findMonorepo(path.resolve(__dirname, "..")).pkgs;
+  config.module.rules.unshift({
+    test: /\.[jt]sx?$/,
+    exclude: /node_modules/,
+    include: [path.resolve(__dirname, ".."), ...monorepoPackages],
     use: [
       {
-        loader: babelLoader.loader,
-        query: babelLoader.query,
+        loader: require.resolve("babel-loader"),
+        options: {
+          ...createBabelConfig(env),
+        },
       },
-      "ts-loader",
-      // {
-      //   loader: "react-docgen-typescript-loader",
-      //   options: {
-      //     includes: ["components.*\\.tsx$"],
-      //     excludes: ["stories\\.tsx$"],
-      //   },
-      // },
-    ],
+      env === "PRODUCTION"
+        ? {
+            loader: require.resolve("react-docgen-typescript-loader"),
+            options: {
+              includes: ["components.*\\.tsx$"],
+              excludes: ["stories\\.tsx$"],
+            },
+          }
+        : false,
+    ].filter(r => Boolean(r)),
   });
+
+  // Add TypeScript definitions.
   config.resolve.extensions.push(".ts", ".tsx");
 
-  return defaultConfig;
+  return config;
 };
+
+// Based on Babel 6 config for Storybook, just converted to Babel 7.
+// https://github.com/storybooks/storybook/blob/master/app/react/src/server/config/babel.js
+//
+// Also added:
+// * babel-plugin-named-asset-import for support for SVGs as React Components
+//   per Create React App 2.
+// * @babel/preset-typescript for TypeScript support.
+function createBabelConfig(env) {
+  return {
+    babelrc: false,
+    presets: [
+      [
+        require.resolve("@babel/preset-env"),
+        {
+          targets: {
+            browsers: ["last 2 versions", "safari >= 7"],
+          },
+          modules: process.env.NODE_ENV === "test" ? "commonjs" : false,
+        },
+      ],
+      [
+        require.resolve("@babel/preset-stage-0"),
+        {
+          decoratorsLegacy: true,
+        },
+      ],
+      require.resolve("@babel/preset-react"),
+      require.resolve("@babel/preset-typescript"),
+      env === "PRODUCTION" ? require.resolve("babel-preset-minify") : false,
+    ].filter(p => Boolean(p)),
+
+    plugins: [
+      [
+        require.resolve("babel-plugin-named-asset-import"),
+        {
+          loaderMap: {
+            svg: {
+              ReactComponent: "svgr/webpack![path]",
+            },
+          },
+        },
+      ],
+      require.resolve("@babel/plugin-transform-regenerator"),
+      [
+        require.resolve("@babel/plugin-transform-runtime"),
+        {
+          helpers: true,
+          polyfill: true,
+          regenerator: true,
+        },
+      ],
+    ],
+  };
+}
