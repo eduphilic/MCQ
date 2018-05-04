1d136992b7a0db059713f6a14395f2fc214b3932
strothj jason@jasons.io <jason@jasons.io>
2018-05-04 05:09:01 -0400

    feat: add entry table

--- packages/app/src/pages/admin/AdminEntryManager.tsx
+++ packages/app/src/pages/admin/AdminEntryManager.tsx
@@ -1,13 +1,19 @@
 import React, { SFC } from "react";
+import styled from "styled";
 import { AdminDashboardTemplateContainer } from "../../containers/AdminDashboardTemplateContainer";
 
 import AppBar from "material-ui/AppBar";
 import Card, { CardHeader } from "material-ui/Card";
+import Chip from "material-ui/Chip";
+import { FormControlLabel } from "material-ui/Form";
 import Icon from "material-ui/Icon";
 import IconButton from "material-ui/IconButton";
+import Switch from "material-ui/Switch";
+import Table, { TableBody, TableCell } from "material-ui/Table";
 import Toolbar from "material-ui/Toolbar";
 
 import { Button } from "components/atoms/Button";
+import { DashboardTableRow } from "components/atoms/DashboardTableRow";
 import { Typography } from "components/atoms/Typography";
 
 export const AdminEntryManager: SFC<{}> = () => {
@@ -45,9 +51,11 @@ export const AdminEntryManager: SFC<{}> = () => {
       {/* Entry Cards */}
       {data.map(entryRecord => {
         const entryTitle = entryRecord[0] as string;
+        const rows = entryRecord.slice(1);
 
         return (
           <Card key={entryTitle}>
+            {/* Entry Card Header */}
             <CardHeader
               title={
                 <div style={{ display: "flex" }}>
@@ -60,14 +68,79 @@ export const AdminEntryManager: SFC<{}> = () => {
                 </div>
               }
               action={
-                <IconButton style={{ paddingTop: 4 }}>
+                <IconButton style={{ marginTop: 4, marginBottom: -4 }}>
                   <Icon>mode_edit</Icon>
                 </IconButton>
               }
             />
+
+            {/* Entry Listings */}
+            <div style={{ overflowX: "auto" }}>
+              <Table style={{ minWidth: 1000 }}>
+                <TableBody>
+                  {rows.map(([category, subcategory, ppp, activated]) => (
+                    <DashboardTableRow key={category as string}>
+                      {/* Category label */}
+                      <TableCell>
+                        <Typography>{category}</Typography>
+                      </TableCell>
+
+                      {/* Chip: category / sub-category */}
+                      <TableCell>
+                        {subcategory ? <ChipSubcategory /> : <ChipCategory />}
+                      </TableCell>
+
+                      {/* Price per paper */}
+                      <TableCell>
+                        <Typography>Rs {ppp} pp</Typography>
+                      </TableCell>
+
+                      {/* Activation Switch */}
+                      <TableCell>
+                        <FormControlLabel
+                          control={
+                            <Switch
+                              color="primary"
+                              checked={activated as boolean}
+                              onClick={() => alert("Placeholder")}
+                            />
+                          }
+                          label={
+                            <Typography>
+                              {activated ? "Activated" : "De-activated"}
+                            </Typography>
+                          }
+                        />
+                      </TableCell>
+
+                      {/* Delete button */}
+                      <TableCell numeric>
+                        <IconButton>
+                          <Icon>delete</Icon>
+                        </IconButton>
+                      </TableCell>
+                    </DashboardTableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
           </Card>
         );
       })}
     </AdminDashboardTemplateContainer>
   );
 };
+
+const ChipCategory = styled(Chip).attrs({
+  label: "category",
+})`
+  color: #fff;
+  background-color: #3fc439;
+`;
+
+const ChipSubcategory = styled(Chip).attrs({
+  label: "sub-category",
+})`
+  color: #fff;
+  background-color: #f299fa;
+`;
--- packages/components/atoms/DashboardTableRow/index.tsx
+++ packages/components/atoms/DashboardTableRow/index.tsx
@@ -5,4 +5,8 @@ export const DashboardTableRow = styled(TableRow)`
   &:nth-of-type(odd) {
     background-color: ${({ theme }) => theme.palette.background.default};
   }
+
+  td {
+    border: none;
+  }
 `;
