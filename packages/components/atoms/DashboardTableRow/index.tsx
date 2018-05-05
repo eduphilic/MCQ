4163a6e0b5955d1929459fa43f712a12ccadb544
strothj jason@jasons.io <jason@jasons.io>
2018-05-05 07:31:20 -0400

    fix: do not make classes prop required

--- packages/components/atoms/DashboardTableRow/index.tsx
+++ packages/components/atoms/DashboardTableRow/index.tsx
@@ -1,10 +1,15 @@
-import { TableRow } from "material-ui/Table";
+import { TableRow, TableRowProps } from "material-ui/Table";
 import styled from "styled";
 
 export const DashboardTableRow = styled(TableRow).attrs({
-  classes: {
-    selected: "selected",
-  },
+  // Used type assertion here because dependent types were requiring a classes
+  // field be set. Using the type from TableRowProps to make it optional.
+  // tslint:disable-next-line:no-object-literal-type-assertion
+  ...({
+    classes: {
+      selected: "selected",
+    },
+  } as Pick<TableRowProps, "classes">),
 })`
   &:nth-of-type(odd) {
     background-color: ${({ theme }) => theme.palette.background.default};
