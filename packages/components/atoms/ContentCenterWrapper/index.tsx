6489cbf932ff8f151a3e140a958659ec44cfe9b8
strothj jason@jasons.io <jason@jasons.io>
2018-05-05 01:01:14 -0400

    feat: export directly the content center wrapper component
    The original intension was to closely control what could be done with
    this component but it makes more sense to export the Styled Component
    directly to make it easier to override styles in place.

--- packages/components/atoms/ContentCenterWrapper/index.tsx
+++ packages/components/atoms/ContentCenterWrapper/index.tsx
@@ -1,23 +1,12 @@
-import React, { SFC } from "react";
 import styled from "styled";
-
-const ContentCenterWrapperBase = styled.div`
-  width: 100%;
-  max-width: ${props => props.theme.breakpoints.values.lg}px;
-  padding: 0 ${props => props.theme.spacing.unit * 2}px;
-  margin: 0 auto;
-`;
-
-interface Props {
-  className?: string;
-}
 
 /**
  * Container with a maximum width, centered on the screen. Includes inner
  * padding.
  */
-export const ContentCenterWrapper: SFC<Props> = ({ children, className }) => (
-  <ContentCenterWrapperBase className={className}>
-    {children}
-  </ContentCenterWrapperBase>
-);
+export const ContentCenterWrapper = styled.div`
+  width: 100%;
+  max-width: ${props => props.theme.breakpoints.values.lg}px;
+  padding: 0 ${props => props.theme.spacing.unit * 2}px;
+  margin: 0 auto;
+`;
