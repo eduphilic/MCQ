b65b688b9d8ccd8f349ae43bd705faec8cdbd5c2
strothj jason@jasons.io <jason@jasons.io>
2018-05-05 07:04:16 -0400

    feat: darken selection background to add more contrast
    Because this component already darkens alternating lines, it makes it
    difficult to see what rows are selected. This change further darkens
    selected rows and makes sure the custom background color doesn't
    override the selected background style.

--- packages/components/atoms/DashboardTableRow/index.tsx
+++ packages/components/atoms/DashboardTableRow/index.tsx
@@ -1,9 +1,17 @@
 import { TableRow } from "material-ui/Table";
 import styled from "styled";
 
-export const DashboardTableRow = styled(TableRow)`
+export const DashboardTableRow = styled(TableRow).attrs({
+  classes: {
+    selected: "selected",
+  },
+})`
   &:nth-of-type(odd) {
     background-color: ${({ theme }) => theme.palette.background.default};
+  }
+
+  &.selected {
+    background-color: rgba(0, 0, 0, 0.07);
   }
 
   td {
