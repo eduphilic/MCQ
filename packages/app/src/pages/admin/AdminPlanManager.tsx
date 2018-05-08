ce6b512b1911619aef5d97331a54e87addffdfab
strothj jason@jasons.io <jason@jasons.io>
2018-05-08 15:52:29 -0400

    feat: add action button to admin plan manager

--- packages/app/src/pages/admin/AdminPlanManager.tsx
+++ packages/app/src/pages/admin/AdminPlanManager.tsx
@@ -1,11 +1,26 @@
 import React, { SFC } from "react";
+
+import Add from "@material-ui/icons/Add";
+
+import { ResponsiveToolbarTypographyButton } from "components/molecules/ResponsiveToolbarTypographyButton";
 import { AdminDashboardTemplateContainer } from "../../containers/AdminDashboardTemplateContainer";
 
 export const AdminPlanManager: SFC<{}> = () => {
-  //
+  const appBarActionButtons = [
+    <ResponsiveToolbarTypographyButton
+      iconNode={<Add />}
+      tooltipTitle="Add New Plan"
+      color="orange"
+    >
+      Plan
+    </ResponsiveToolbarTypographyButton>,
+  ];
 
   return (
-    <AdminDashboardTemplateContainer titleText="Plan Manager">
+    <AdminDashboardTemplateContainer
+      titleText="Plan Manager"
+      actionButtonElements={appBarActionButtons}
+    >
       <div>Placeholder</div>
     </AdminDashboardTemplateContainer>
   );
