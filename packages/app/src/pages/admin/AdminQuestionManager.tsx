7509c4c447a4edb1b3576c6bc94b0af16a813621
strothj jason@jasons.io <jason@jasons.io>
2018-05-08 09:25:47 -0400

    feat: add secondary toolbar

--- packages/app/src/pages/admin/AdminQuestionManager.tsx
+++ packages/app/src/pages/admin/AdminQuestionManager.tsx
@@ -1,12 +1,31 @@
 import React, { SFC } from "react";
 import { AdminDashboardTemplateContainer } from "../../containers/AdminDashboardTemplateContainer";
 
+import { Button } from "components/atoms/Button";
+import { Typography } from "components/atoms/Typography";
+import { DashboardSecondaryToolbar } from "components/molecules/DashboardSecondaryToolbar";
+
 export const AdminQuestionManager: SFC<{}> = () => {
   //
 
   return (
     <AdminDashboardTemplateContainer titleText="Question Manager">
-      <div>Placeholder</div>
+      {/* Toolbar */}
+      <DashboardSecondaryToolbar>
+        <DashboardSecondaryToolbar.Spacer />
+
+        <Button color="primary" variant="flat">
+          <Typography variant="buttonBold">Add Questions</Typography>
+        </Button>
+        <Button color="orange" variant="flat">
+          <Typography variant="buttonBold">Import Questions</Typography>
+        </Button>
+        <Button color="orange" variant="flat">
+          <Typography>Challenged Questions</Typography>
+        </Button>
+      </DashboardSecondaryToolbar>
+
+      {/* */}
     </AdminDashboardTemplateContainer>
   );
 };
