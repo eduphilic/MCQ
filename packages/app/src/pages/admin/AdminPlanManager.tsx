22c1fce6dbc70bcd0c1cde2e11dd5e22e68bef7c
strothj jason@jasons.io <jason@jasons.io>
2018-05-09 16:37:38 -0400

    feat: populate plan manager

--- packages/app/src/pages/admin/AdminPlanManager.tsx
+++ packages/app/src/pages/admin/AdminPlanManager.tsx
@@ -3,6 +3,7 @@ import React, { SFC } from "react";
 import Add from "@material-ui/icons/Add";
 
 import { ResponsiveToolbarTypographyButton } from "components/molecules/ResponsiveToolbarTypographyButton";
+import { DashboardCard } from "components/organisms/DashboardCard";
 import { AdminDashboardTemplateContainer } from "../../containers/AdminDashboardTemplateContainer";
 
 export const AdminPlanManager: SFC<{}> = () => {
@@ -16,12 +17,55 @@ export const AdminPlanManager: SFC<{}> = () => {
     </ResponsiveToolbarTypographyButton>,
   ];
 
+  const noop = () => {}; // tslint:disable-line
+
   return (
     <AdminDashboardTemplateContainer
       titleText="Plan Manager"
       actionButtonElements={appBarActionButtons}
     >
-      <div>Placeholder</div>
+      {["Plans"].map(title => (
+        <DashboardCard
+          key={title}
+          title={title}
+          columnLabels={["Plan", "Availability"]}
+          columnTypes={["single-line", "switch"]}
+          onItemEditClick={noop}
+          onRequestDeleteClick={noop}
+          items={[
+            {
+              key: "0",
+              columns: [
+                { primaryText: "5 Exam Plan" },
+                {
+                  switchChecked: false,
+                  switchTooltipTitle: "Toggle Availability",
+                },
+              ],
+            },
+            {
+              key: "1",
+              columns: [
+                { primaryText: "10 Exam Plan" },
+                {
+                  switchChecked: true,
+                  switchTooltipTitle: "Toggle Availability",
+                },
+              ],
+            },
+            {
+              key: "2",
+              columns: [
+                { primaryText: "15 Exam Plan" },
+                {
+                  switchChecked: false,
+                  switchTooltipTitle: "Toggle Availability",
+                },
+              ],
+            },
+          ]}
+        />
+      ))}
     </AdminDashboardTemplateContainer>
   );
 };
