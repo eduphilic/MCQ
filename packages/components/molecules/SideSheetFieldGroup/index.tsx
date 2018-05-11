ac770ed6b9e255c691b5dbc0d7171ad10ad176c8
strothj jason@jasons.io <jason@jasons.io>
2018-05-11 18:43:03 -0400

    feat: add side sheet field group component

---
+++ packages/components/molecules/SideSheetFieldGroup/index.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import styled from "styled";
+
+import { Typography } from "../../atoms/Typography";
+
+export interface SideSheetFieldGroupProps {
+  /**
+   * Text for the field group title.
+   */
+  fieldGroupTitle?: string;
+}
+
+export class SideSheetFieldGroup extends Component<SideSheetFieldGroupProps> {
+  render() {
+    const { children, fieldGroupTitle } = this.props;
+
+    return (
+      <Wrapper>
+        {fieldGroupTitle && (
+          <Typography variant="formFieldTitle">{fieldGroupTitle}</Typography>
+        )}
+
+        {children}
+      </Wrapper>
+    );
+  }
+}
+
+const Wrapper = styled.div`
+  &:not(:last-child) {
+    margin-bottom: ${({ theme }) => theme.spacing.unit * 2}px;
+  }
+`;
