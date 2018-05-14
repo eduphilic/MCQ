75475146cc03d8a50318628e98bd914dbf3c0ce0
strothj jason@jasons.io <jason@jasons.io>
2018-05-14 18:22:41 -0400

    feat: add dashboard card pagination component

---
+++ packages/components/organisms/DashboardCard/DashboardCardPagination.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import { TablePagination, TablePaginationProps } from "material-ui/Table";
+
+// tslint:disable-next-line:no-empty-interface
+export interface DashboardCardPaginationProps extends TablePaginationProps {}
+
+export class DashboardCardPagination extends Component<
+  DashboardCardPaginationProps
+> {
+  render() {
+    return <TablePagination {...this.props} />;
+  }
+}
