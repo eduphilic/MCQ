1659770616b3d55f8eca42de9bbee1cc27a32eed
strothj jason@jasons.io <jason@jasons.io>
2018-05-21 23:50:39 -0400

    feat: start initial implementation of test manager new template page

--- packages/app/src/pages/admin/AdminTestManagerNewTemplate.tsx
+++ packages/app/src/pages/admin/AdminTestManagerNewTemplate.tsx
@@ -1,4 +1,12 @@
+import { Formik } from "formik";
 import React, { Component } from "react";
+
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+
+import { Typography } from "components/atoms/Typography";
+
+import { AdminDashboardTemplateContainer } from "../../containers/AdminDashboardTemplateContainer";
 
 // tslint:disable-next-line:no-empty-interface
 export interface AdminTestManagerNewTemplateProps {}
@@ -7,10 +15,36 @@ export class AdminTestManagerNewTemplate extends Component<
   AdminTestManagerNewTemplateProps
 > {
   render() {
+    const initialValues = {
+      entryType: "",
+    };
+
+    type Values = typeof initialValues;
+
+    // tslint:disable-next-line:max-classes-per-file
+    const FormikTyped = class extends Formik<{}, Values> {};
+
     return (
-      <>
-        <div>Placeholder</div>
-      </>
+      <FormikTyped
+        initialValues={initialValues}
+        onSubmit={() => alert("Form submission")}
+      >
+        {_api => {
+          return (
+            <AdminDashboardTemplateContainer titleText="Test Manager > Add New Template">
+              <Card>
+                <CardHeader
+                  title={
+                    <Typography variant="cardTitle">
+                      Basic Information
+                    </Typography>
+                  }
+                />
+              </Card>
+            </AdminDashboardTemplateContainer>
+          );
+        }}
+      </FormikTyped>
     );
   }
 }
