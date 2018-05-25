57654c0fa068c2e34dab8886da9c6ff0c9b39085
strothj jason@jasons.io <jason@jasons.io>
2018-05-25 00:56:38 -0400

    feat: add adjustment for typings for @storybook/addon-info

--- typings/storybook__addon-info/index.d.ts
+++ typings/storybook__addon-info/index.d.ts
@@ -1,10 +1,36 @@
-import { WrapStoryProps } from "@storybook/addon-info";
+import { WrapStoryProps, Options } from "@storybook/addon-info";
 import { RenderFunction } from "@storybook/react";
-import { ReactElement } from "react";
+import { ReactElement, ComponentType } from "react";
 
 // Add missing function form where options are optional.
 declare module "@storybook/addon-info" {
   export function withInfo(): (
     storyFn: RenderFunction,
   ) => () => ReactElement<WrapStoryProps>;
+
+  // Use corrected options interface, see below.
+  export function withInfo(
+    options: OptionsCorrected,
+  ): (
+    storyFn: RenderFunction,
+  ) => (context?: object) => React.ReactElement<WrapStoryProps>;
+
+  // The props "propTables" and "propTablesExclude" are incorrectly defined.
+  // Here we define a new Options interface with corrected definitions.
+  export interface OptionsCorrected
+    extends Omit<Options, "propTables" | "propTablesExclude"> {
+    propTables?: ComponentType<any>[];
+    propTablesExclude?: ComponentType<any>[];
+  }
 }
+
+// Opposite of Pick.
+// Ref: http://ideasintosoftware.com/typescript-advanced-tricks/
+
+type Diff<T extends string, U extends string> = ({ [P in T]: P } &
+  { [P in U]: never } & { [x: string]: never })[T];
+
+/**
+ * Remove a field from a type definition.
+ */
+type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>;
