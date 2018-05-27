9f6cf54d1bfac04b6c5286d3c4f9d394fee68629
strothj jason@jasons.io <jason@jasons.io>
2018-05-27 01:13:03 -0400

    feat: add initial filter groups

--- packages/app/src/pages/admin/AdminUserManager.tsx
+++ packages/app/src/pages/admin/AdminUserManager.tsx
@@ -1,11 +1,48 @@
 import React, { SFC } from "react";
+
 import { AdminDashboardTemplateContainer } from "../../containers/AdminDashboardTemplateContainer";
+
+import { SideSheetButtonMenu } from "components/molecules/SideSheetButtonMenu";
+import { SideSheetFieldGroup } from "components/molecules/SideSheetFieldGroup";
 
 export const AdminUserManager: SFC<{}> = () => {
   //
 
   return (
-    <AdminDashboardTemplateContainer titleText="User Manager">
+    <AdminDashboardTemplateContainer
+      titleText="User Manager"
+      sideSheetContents={
+        <>
+          <SideSheetFieldGroup fieldGroupTitle="Users">
+            <SideSheetButtonMenu
+              options={[
+                "All Users",
+                "Active Users",
+                "Inactive Users",
+                "Deleted Users",
+              ]}
+            />
+          </SideSheetFieldGroup>
+
+          <SideSheetFieldGroup fieldGroupTitle="Entries">
+            <SideSheetButtonMenu options={["All Entries"]} />
+          </SideSheetFieldGroup>
+
+          <SideSheetFieldGroup fieldGroupTitle="Plans">
+            <SideSheetButtonMenu
+              options={[
+                "All Plans",
+                "Trial Pack",
+                "5 Paper Plan",
+                "10 Paper Plan",
+                "15 Paper Plan",
+                "20 Paper Plan",
+              ]}
+            />
+          </SideSheetFieldGroup>
+        </>
+      }
+    >
       <div>Placeholder</div>
     </AdminDashboardTemplateContainer>
   );
