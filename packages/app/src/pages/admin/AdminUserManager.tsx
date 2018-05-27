bf4fcf879ce34330f8fae45810067afe91a3b4ab
strothj jason@jasons.io <jason@jasons.io>
2018-05-27 04:29:54 -0400

    feat: add user manager column data

--- packages/app/src/pages/admin/AdminUserManager.tsx
+++ packages/app/src/pages/admin/AdminUserManager.tsx
@@ -5,9 +5,47 @@ import { AdminDashboardTemplateContainer } from "../../containers/AdminDashboard
 import { SideSheetButtonMenu } from "components/molecules/SideSheetButtonMenu";
 import { SideSheetFieldGroup } from "components/molecules/SideSheetFieldGroup";
 import { SideSheetSearchField } from "components/molecules/SideSheetSearchField";
+import {
+  DashboardCard,
+  DashboardCardColumnType,
+  DashboardCardItem,
+} from "components/organisms/DashboardCard";
+
+// tslint:disable-next-line:import-name
+import profileImage from "./admin-user-manager-placeholder-profile-image.png";
 
 export const AdminUserManager: SFC<{}> = () => {
-  //
+  const cardItems = Array.from({ length: 30 }).map(
+    (_item, index) =>
+      // tslint:disable-next-line:no-object-literal-type-assertion
+      ({
+        key: index.toString(),
+        columns: [
+          {
+            imgUrl: profileImage,
+            primaryText: "User Name",
+            secondaryText: "emailed@gmail.com",
+          },
+          {
+            primaryText: "14 Jan 2018",
+          },
+          {
+            primaryText: "Trial Pack",
+            secondaryText: "GD, Sol Tech",
+          },
+          {
+            primaryText: "14 Mar 18",
+          },
+        ],
+      } as DashboardCardItem),
+  );
+  const cardColumnLabels = ["User", "Joined On", "Plan", "Expires"];
+  const cardColumnTypes: DashboardCardColumnType[] = [
+    "profile",
+    "single-line",
+    "dual-line",
+    "single-line",
+  ];
 
   return (
     <AdminDashboardTemplateContainer
@@ -51,7 +89,11 @@ export const AdminUserManager: SFC<{}> = () => {
         </>
       }
     >
-      <div>Placeholder</div>
+      <DashboardCard
+        items={cardItems}
+        columnLabels={cardColumnLabels}
+        columnTypes={cardColumnTypes}
+      />
     </AdminDashboardTemplateContainer>
   );
 };
