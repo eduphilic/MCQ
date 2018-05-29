96163089e287173b884b640be788790bdff5e5fe
strothj jason@jasons.io <jason@jasons.io>
2018-05-29 05:04:02 -0400

    feat: add plan manager new plan modal

--- packages/app/src/pages/admin/AdminPlanManager.tsx
+++ packages/app/src/pages/admin/AdminPlanManager.tsx
@@ -4,20 +4,58 @@ import Add from "@material-ui/icons/Add";
 
 import { ResponsiveToolbarTypographyButton } from "components/molecules/ResponsiveToolbarTypographyButton";
 import { DashboardCard } from "components/organisms/DashboardCard";
+import {
+  DashboardFormDialog,
+  FieldConfigs,
+} from "components/organisms/DashboardFormDialog";
 import { AdminDashboardTemplateContainer } from "../../containers/AdminDashboardTemplateContainer";
 
 export const AdminPlanManager: SFC<{}> = () => {
-  const appBarActionButtons = [
-    <ResponsiveToolbarTypographyButton
-      iconNode={<Add />}
-      tooltipTitle="Add New Plan"
-      color="orange"
-    >
-      Plan
-    </ResponsiveToolbarTypographyButton>,
-  ];
-
   const noop = () => {}; // tslint:disable-line
+
+  const dialogFields = {
+    planName: "",
+    isFree: "false",
+    numTests: 5,
+    planValidityDays: 5,
+  };
+
+  const fieldConfigs: FieldConfigs<typeof dialogFields> = {
+    planName: {
+      inputLabel: "Plan Name",
+      inputType: "text",
+      placeholder: "Enter plan name here...",
+    },
+    isFree: {
+      inputLabel: "Make this plan free (TODO: Make Checkmark)",
+      inputType: "text",
+    },
+    numTests: {
+      inputLabel: "Number of test papers (TODO: Make Number Range)",
+      inputType: "text",
+    },
+    planValidityDays: {
+      inputLabel: "Plan Validity (TODO: Make Number Range)",
+      inputType: "text",
+      placeholder: "Enter in number of days",
+    },
+  };
+
+  const appBarActionButtons = [
+    <DashboardFormDialog
+      fieldConfigs={fieldConfigs}
+      initialValues={dialogFields}
+      onSubmit={noop}
+    >
+      <ResponsiveToolbarTypographyButton
+        iconNode={<Add />}
+        tooltipTitle="Add New Plan"
+        color="orange"
+      >
+        Plan
+      </ResponsiveToolbarTypographyButton>
+    </DashboardFormDialog>,
+  ];
 
   return (
     <AdminDashboardTemplateContainer
