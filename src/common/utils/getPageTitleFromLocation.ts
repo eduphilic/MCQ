53d4b48673114bb5ef2202114737c0447c6139a4
strothj jason@jasons.io <jason@jasons.io>
2018-06-09 18:04:38 -0400

    chore: move utils to path under common

---
+++ src/common/utils/getPageTitleFromLocation.ts
@@ -0,0 +1,20 @@
+import { navigationLinksAll } from "common/structures/navigationLinksAll";
+import strings from "l10n";
+
+const defaultOptions = {
+  withoutProductName: false,
+};
+
+export const getPageTitleFromLocation = (
+  location: string,
+  options: Partial<typeof defaultOptions> = defaultOptions,
+): string => {
+  const page = navigationLinksAll.find(l => l.to === location);
+
+  let title = page ? strings[page.titleLocalizationKey] : "";
+  title = `${title}${title.length > 0 ? " - " : ""}JoinUniform`;
+
+  if (options.withoutProductName) title = title.replace(/- JoinUniform$/, "");
+
+  return title;
+};
--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,2 +1,3 @@
+export * from "./getPageTitleFromLocation";
 export * from "./contextStore";
 export * from "./randomNumber";
