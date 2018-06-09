65e56fb26d7b428248a146ee2b2cb962fc365488
strothj jason@jasons.io <jason@jasons.io>
2018-06-09 13:12:34 -0400

    feat: create common interface for navigation for use in nav components

---
+++ src/common/types/NavigationLinks.ts
@@ -0,0 +1,7 @@
+import { NavigationLink } from "./NavigationLink";
+
+/**
+ * List of navigation links to include in a drawer or bottom navigation
+ * component.
+ */
+export type NavigationLinks = NavigationLink[];
