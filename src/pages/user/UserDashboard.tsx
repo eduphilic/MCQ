117ea8bd7820afed18a18fc0f7a92330301400c8
strothj jason@jasons.io <jason@jasons.io>
2018-06-12 00:35:27 -0400

    feat: start populating user dashboard page

--- src/pages/user/UserDashboard.tsx
+++ src/pages/user/UserDashboard.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { entryImages } from "common/structures/entryImages";
+import React, { SFC } from "react";
 
-export const UserDashboard = () => (
-  <>
-    <div>User Dashboard Page Contents</div>
-  </>
-);
+import {
+  DashboardTestCard,
+  DashboardTestCardProps,
+} from "components/DashboardTestCard";
+
+export const UserDashboard: SFC<{}> = () => {
+  const cards: DashboardTestCardProps[] = [
+    {
+      imageLogoUrl: entryImages.AirForce,
+      title: "Soldier General Duty Free Mock Test",
+      subtitle: "Validity 31st Jan 2019",
+      color: "yellow",
+      attemptButtonLabel: "Attempt",
+    },
+    {
+      imageLogoUrl: entryImages.AirForce,
+      title: "Soldier General Duty 10 Mock Tests Set",
+      subtitle: "Validity 31st Jan 2019",
+      attemptButtonLabel: "Attempt",
+    },
+    {
+      imageLogoUrl: entryImages.Army,
+      title: "Airmen Group X 20 Mock Tests Set",
+      subtitle: "Validity 31st Jan 2019",
+      attemptButtonLabel: "Attempt",
+    },
+    {
+      imageLogoUrl: entryImages.AssamRifles,
+      title: "Sailor Airificer Apprentice 20 Mock Tests Set",
+      subtitle: "Validity 31st Jan 2019",
+      attemptButtonLabel: "Attempt",
+    },
+  ];
+
+  return <>{cards.map(c => <DashboardTestCard key={c.title} {...c} />)}</>;
+};
