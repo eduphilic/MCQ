9f5107642388702957abe1b5ed6c0a902f4316fa
strothj jason@jasons.io <jason@jasons.io>
2018-06-12 11:18:38 -0400

    feat: increase z-index of bottom nav so shadow appears over page

--- src/components/DashboardTemplateMobile/index.tsx
+++ src/components/DashboardTemplateMobile/index.tsx
@@ -161,6 +161,7 @@ const Pane = styled.div`
 const PaperWithBoxShadowUpperDirection = styled(Paper)`
   box-shadow: 0px -1px 5px 0px rgba(0, 0, 0, 0.2),
     0px -2px 2px 0px rgba(0, 0, 0, 0.14), 0px -3px 1px -2px rgba(0, 0, 0, 0.12);
+  z-index: 1;
 `;
 
 const BottomNavigationWithBackgroundColor = styled(BottomNavigation)`
