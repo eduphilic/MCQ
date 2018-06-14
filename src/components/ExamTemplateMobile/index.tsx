25b26e20657587d5d7fb8742d3a82b34bac093c1
strothj jason@jasons.io <jason@jasons.io>
2018-06-14 05:55:23 -0400

    feat: add placeholder for exam template mobile component

---
+++ src/components/ExamTemplateMobile/index.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+// tslint:disable-next-line:no-empty-interface
+export interface ExamTemplateMobileProps {}
+
+export class ExamTemplateMobile extends Component<ExamTemplateMobileProps> {
+  render() {
+    return (
+      <>
+        <div>Placeholder</div>
+      </>
+    );
+  }
+}
