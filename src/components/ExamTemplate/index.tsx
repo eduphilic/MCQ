0fae6d1b5f5ee6a9122c56bc3f0d4cd7b326ca2f
strothj jason@jasons.io <jason@jasons.io>
2018-06-14 05:54:23 -0400

    feat: add placeholder content to desktop view of exam template

--- src/components/ExamTemplate/index.tsx
+++ src/components/ExamTemplate/index.tsx
@@ -1,6 +1,8 @@
 import React, { SFC } from "react";
+import { UserAppDrawerTheme } from "theme";
 
 import { DashboardTemplate } from "components/DashboardTemplate";
+import { ExamDrawerContents } from "components/ExamDrawerContents";
 
 // tslint:disable-next-line:no-empty-interface
 export interface ExamTemplateProps {}
@@ -8,10 +10,13 @@ export interface ExamTemplateProps {}
 export const ExamTemplate: SFC<ExamTemplateProps> = props => {
   const {} = props;
 
+  const drawerContentsNode = <ExamDrawerContents />;
+
   return (
     <DashboardTemplate
       appBarNode={<div>Placeholder</div>}
-      drawerContentsNode={<div>Placeholder</div>}
+      drawerContentsNode={drawerContentsNode}
+      drawerThemeElement={<UserAppDrawerTheme />}
     >
       <div>Placeholder</div>
     </DashboardTemplate>
