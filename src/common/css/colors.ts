db07425306c4ffe34206641d1f13ed2b85c66afa
strothj jason@jasons.io <jason@jasons.io>
2018-06-14 02:50:36 -0400

    feat: add color for exam drawer info cards

---
+++ src/common/css/colors.ts
@@ -0,0 +1,7 @@
+/**
+ * Background color for info cards in the exam screens.
+ *
+ * This is the background color for the timer component and the question
+ * palette.
+ */
+export const examDrawerInfoCardBackground = "#1d304b";
