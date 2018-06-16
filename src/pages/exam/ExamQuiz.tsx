c708c2a355016c6831b15ce4d52dde148b87c536
strothj jason@jasons.io <jason@jasons.io>
2018-06-16 04:25:08 -0400

    feat: add exam blueprint to exam page

--- src/pages/exam/ExamQuiz.tsx
+++ src/pages/exam/ExamQuiz.tsx
@@ -1,12 +1,22 @@
 import React, { SFC } from "react";
 
+import { DashboardColumnContainer } from "components/DashboardColumnContainer";
+import { ExamBluePrint } from "components/ExamBluePrint";
 import { ExamNavigationStorePlaceholderConsumer } from "components/ExamNavigationStorePlaceholder";
 import { ExamTemplate } from "components/ExamTemplate";
 
 export const ExamQuiz: SFC<{}> = () => {
   return (
     <ExamNavigationStorePlaceholderConsumer>
-      {() => <ExamTemplate />}
+      {({ page }) => (
+        <ExamTemplate>
+          {page === "overview" && (
+            <DashboardColumnContainer>
+              {[<ExamBluePrint key="blue-print" />]}
+            </DashboardColumnContainer>
+          )}
+        </ExamTemplate>
+      )}
     </ExamNavigationStorePlaceholderConsumer>
   );
 };
