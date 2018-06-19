d036b7a2399c8339ddfcfb4bf64cd51c740ee714
strothj jason@jasons.io <jason@jasons.io>
2018-06-19 15:16:13 -0400

    feat: make bottom nav shadow reusable

--- src/components/DashboardTemplateMobile/index.tsx
+++ src/components/DashboardTemplateMobile/index.tsx
@@ -1,3 +1,4 @@
+import { bottomNavBoxShadow } from "common/css/bottomNavBoxShadow";
 import { NavigationLinks } from "common/types/NavigationLinks";
 import strings from "l10n";
 import React, {
@@ -130,8 +131,7 @@ const DashboardTemplateMobileWithRouter = withRouter(DashboardTemplateMobile);
 export { DashboardTemplateMobileWithRouter as DashboardTemplateMobile };
 
 const PaperWithBoxShadowUpperDirection = styled(Paper)`
-  box-shadow: 0px -1px 5px 0px rgba(0, 0, 0, 0.2),
-    0px -2px 2px 0px rgba(0, 0, 0, 0.14), 0px -3px 1px -2px rgba(0, 0, 0, 0.12);
+  ${bottomNavBoxShadow};
   z-index: 1;
 `;
 
