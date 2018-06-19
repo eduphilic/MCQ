7d4798a0e5af25f7d6e1e1298062bf1205d8d002
strothj jason@jasons.io <jason@jasons.io>
2018-06-19 15:43:40 -0400

    feat: add optional generator function to toolbar height css generator

--- src/common/css/fromToolbarHeight.ts
+++ src/common/css/fromToolbarHeight.ts
@@ -1,14 +1,19 @@
 import { CSSProperties } from "react";
 import { css } from "styled";
 
-export const fromToolbarHeight = (property: keyof CSSProperties) => css`
-  ${property}: 56px;
+const defaultValueBuilder = (height: number) => `${height}px`;
+
+export const fromToolbarHeight = (
+  property: keyof CSSProperties,
+  valueBuilder = defaultValueBuilder,
+) => css`
+  ${property}: ${valueBuilder(56)};
 
   ${({ theme }) => theme.breakpoints.up("xs")} {
-    ${property}: 48px;
+    ${property}: ${valueBuilder(48)};
   }
 
   ${({ theme }) => theme.breakpoints.up("sm")} {
-    ${property}: 64px;
+    ${property}: ${valueBuilder(64)};
   }
 `;
