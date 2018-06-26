51731a0d2e196b257bc24e9e6deb8dd46b4c6210
strothj jason@jasons.io <jason@jasons.io>
2018-06-26 04:56:01 -0400

    feat: update story to use storybook content wrapper

--- src/exam-taking/components/ExamHeader/ExamHeader.stories.tsx
+++ src/exam-taking/components/ExamHeader/ExamHeader.stories.tsx
@@ -1,11 +1,16 @@
 import { storiesOf } from "@storybook/react";
 import React from "react";
 
+import { StorybookContentCenterWrapper } from "components/storybook/StorybookContentCenterWrapper";
 import { ExamHeader } from "./";
 import { examHeaderPlaceholderProps } from "./ExamHeader.placeholder";
 
 storiesOf("Exam Taking", module).add("ExamHeader", () => {
   //
 
-  return <ExamHeader {...examHeaderPlaceholderProps} />;
+  return (
+    <StorybookContentCenterWrapper>
+      <ExamHeader {...examHeaderPlaceholderProps} />
+    </StorybookContentCenterWrapper>
+  );
 });
