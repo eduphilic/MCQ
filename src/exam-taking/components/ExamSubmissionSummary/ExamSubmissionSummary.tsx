eceb4e2deb10d0d3f3a8bc0b1846f5daf573e0c1
strothj jason@jasons.io <jason@jasons.io>
2018-06-29 04:17:04 -0400

    feat: add submission buttons

--- src/exam-taking/components/ExamSubmissionSummary/ExamSubmissionSummary.tsx
+++ src/exam-taking/components/ExamSubmissionSummary/ExamSubmissionSummary.tsx
@@ -5,6 +5,7 @@ import React, { ReactNode, SFC } from "react";
 import styled from "styled";
 
 import { Typography } from "components/Typography";
+import { TypographyButton } from "components/TypographyButton";
 
 export interface ExamStats {
   totalQuestionsCount: number;
@@ -41,6 +42,23 @@ export const ExamSubmissionSummary: SFC<ExamSubmissionSummaryProps> = props => {
           <Typography variant="examTitle">{value}</Typography>
         </StatWrapper>
       ))}
+
+      <QuestionAndButtonsWrapper
+        questionNode={
+          <Typography
+            variant="examTitle"
+            muiTypographyProps={{ color: "error" }}
+          >
+            Are you sure you want to submit?
+          </Typography>
+        }
+        buttonsNode={
+          <>
+            <TypographyButton>Submit</TypographyButton>
+            <TypographyButton>Attempt Balance</TypographyButton>
+          </>
+        }
+      />
     </Wrapper>
   );
 };
@@ -73,6 +91,33 @@ const StatWrapper = styled.div`
   ${({ theme }) => theme.breakpoints.up("md")} {
     display: inline-flex;
     width: calc(25% - ${({ theme }) => theme.spacing.unit}px);
+  }
+`;
+
+const QuestionAndButtonsWrapper = styled<{
+  className?: string;
+  children?: never;
+  questionNode: ReactNode;
+  buttonsNode: ReactNode;
+}>(({ className, questionNode, buttonsNode }) => (
+  <div className={className}>
+    {questionNode}
+
+    <div className="buttons">{buttonsNode}</div>
+  </div>
+))`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  margin-top: 16px;
+
+  .buttons {
+    margin-top: 16px;
+  }
+
+  .buttons > *:not(:last-child) {
+    margin-right: 16px;
   }
 `;
 
