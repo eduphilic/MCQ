23d0de79429a962ba62420948079732d38014baa
strothj jason@jasons.io <jason@jasons.io>
2018-07-01 07:03:31 -0400

    feat: move redux provider to outside of hot reload path

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import React from "react";
 import { hot } from "react-hot-loader";
-import { Provider } from "react-redux";
-
-import { createStore } from "store";
 import { LightTheme, ThemeBaseline } from "theme";
 
 import { SiteMap } from "./pages";
 
-const store = createStore();
-
 const App = () => (
-  <Provider store={store}>
-    <ThemeBaseline>
-      <LightTheme>
-        <SiteMap />
-      </LightTheme>
-    </ThemeBaseline>
-  </Provider>
+  <ThemeBaseline>
+    <LightTheme>
+      <SiteMap />
+    </LightTheme>
+  </ThemeBaseline>
 );
 
 export default hot(module)(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { hydrate, render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "store";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root")!;
 const renderFunc = rootElement.hasChildNodes() ? hydrate : render;
 
-renderFunc(<App />, document.getElementById("root"));
+const store = createStore();
+
+renderFunc(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root"),
+);
