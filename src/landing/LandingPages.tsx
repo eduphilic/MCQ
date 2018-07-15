54fff70ae19942bee15228b2ed0f4e5ce0ebd963
strothj jason@jasons.io <jason@jasons.io>
2018-07-15 05:16:13 -0400

    fix: only landing page routes rendered

--- src/landing/LandingPages.tsx
+++ src/landing/LandingPages.tsx
@@ -1,23 +1,24 @@
-import React, { SFC } from "react";
+import React from "react";
 import { Route } from "react-router-dom";
 import { routePathFromLocalizationKey } from "../routes";
 
 import { LandingPage } from "./LandingPage";
 import { PasswordResetPage } from "./PasswordResetPage";
 
-export const LandingPages: SFC<{}> = () => (
-  <>
+export const LandingPages = () =>
+  [
     <Route
+      key="landing"
       path={routePathFromLocalizationKey("routes_pages_Landing_LandingHome")}
       exact
       component={LandingPage}
-    />
+    />,
 
     <Route
+      key="password"
       path={routePathFromLocalizationKey(
         "routes_pages_Landing_LandingPasswordReset",
       )}
       component={PasswordResetPage}
-    />
-  </>
-);
+    />,
+  ] as any;
