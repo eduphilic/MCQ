0f7136d0011b6f20f995346eb6ee3a47e33e6d29
strothj jason@jasons.io <jason@jasons.io>
2018-07-15 03:38:43 -0400

    feat: remove unused user template component

--- src/components/DashboardTemplateMobile/index.stories.tsx
+++ src/components/DashboardTemplateMobile/index.stories.tsx
@@ -1,21 +1,18 @@
 import { storiesOf } from "@storybook/react";
-import { navigationLinksUser } from "common/structures/navigationLinksUser";
+import { navigationLinksAdmin } from "common/structures/navigationLinksAdmin";
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 
 import { DashboardTemplateMobile } from ".";
-import { generateTemplateProps } from "../UserTemplate/generateTemplateProps";
 
 const stories = storiesOf("Components", module);
 
 stories.add("DashboardTemplateMobile", () => {
-  const { appBarNode } = generateTemplateProps({ showHamburgerButton: false });
-
   return (
     <MemoryRouter initialEntries={["/dashboard"]}>
       <DashboardTemplateMobile
-        appBarNode={appBarNode}
-        links={navigationLinksUser}
+        appBarNode={() => <div>App Bar</div>}
+        links={navigationLinksAdmin}
       />
     </MemoryRouter>
   );
