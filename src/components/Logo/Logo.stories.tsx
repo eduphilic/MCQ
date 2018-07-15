e8dac5b07de59fce0f13b3ade6f27a222aea23f9
strothj jason@jasons.io <jason@jasons.io>
2018-07-15 05:02:22 -0400

    feat: merge logo and logo text components, add second coloring option

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -1,14 +1,19 @@
 import { withInfo } from "@storybook/addon-info";
+import { boolean } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import { SplitThemesPreviewer } from "components/SplitThemesPreviewer";
 import React from "react";
-import { LogoText } from ".";
+
+import { Logo } from "./Logo";
 
 storiesOf("Components", module).add(
-  "LogoText",
-  withInfo({ propTablesExclude: [SplitThemesPreviewer as any] })(() => (
+  "Logo",
+  withInfo()(() => (
     <SplitThemesPreviewer>
-      <LogoText />
+      <Logo
+        hideTextMobile={boolean("Hide text on mobile", false)}
+        alternateSecondWordColoring={boolean("Alternate coloring", true)}
+      />
     </SplitThemesPreviewer>
   )),
 );
