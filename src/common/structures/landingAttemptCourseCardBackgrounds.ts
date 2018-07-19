801f7da673acec4bae9ead6b6241cf0e3739bdab
strothj jason@jasons.io <jason@jasons.io>
2018-07-19 03:00:53 -0400

    feat: remove unused placeholder attempt course card data

--- src/common/structures/landingAttemptCourseCardBackgrounds.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Filenames of index card background images for use on the landing page in the
- * AttemptCourseCard components.
- */
-export const landingAttemptCourseCardBackgrounds = [
-  "air force1.png",
-  "army1.png",
-  "assam rifles1.png",
-].map(f => encodeURIComponent(f));
