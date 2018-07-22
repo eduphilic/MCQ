8c4bb977296296f0c3ad8229a97997379e4ccf21
strothj jason@jasons.io <jason@jasons.io>
2018-07-22 14:21:17 -0400

    feat: theme bottom toolbar to match user dashboard

--- src/subscription-management/components/BottomToolbar/BottomToolbar.tsx
+++ src/subscription-management/components/BottomToolbar/BottomToolbar.tsx
@@ -1,16 +1,43 @@
-import React, { SFC } from "react";
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { routePathFromLocalizationKey } from "routes";
 import styled from "styled";
 
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 
-export const BottomToolbar: SFC<{}> = props => (
-  <AppBar position="static" color="default">
-    <Toolbar>
-      <ToolbarContentsWrapper>{props.children}</ToolbarContentsWrapper>
-    </Toolbar>
-  </AppBar>
-);
+const dashboardRoutes = [
+  routePathFromLocalizationKey("routes_Dashboard_MembershipEntriesPage"),
+  routePathFromLocalizationKey("routes_Dashboard_MembershipSubscriptionPage"),
+];
+
+export const BottomToolbar = withRouter(props => {
+  const useDashboardBackgroundColor = dashboardRoutes.includes(
+    props.location.pathname,
+  );
+
+  return (
+    <StyledAppBar
+      position="static"
+      color="default"
+      className={
+        useDashboardBackgroundColor ? "dashboard-background-color" : ""
+      }
+    >
+      <Toolbar>
+        <ToolbarContentsWrapper>{props.children}</ToolbarContentsWrapper>
+      </Toolbar>
+    </StyledAppBar>
+  );
+});
+
+const StyledAppBar = styled(AppBar)`
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    &.dashboard-background-color {
+      background-color: #03285b !important;
+    }
+  }
+`;
 
 const ToolbarContentsWrapper = styled.div`
   display: flex;
