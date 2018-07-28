4b8398da71e76352ac034e30c2a76d7c19b13904
strothj jason@jasons.io <jason@jasons.io>
2018-07-28 09:39:07 -0400

    feat(exam-review): add entries placeholder data creator

---
+++ src/exam-review/placeholders/createEntryPlaceholders.ts
@@ -0,0 +1,20 @@
+import { IEntry } from "models";
+
+export const createEntryPlaceholders = (): IEntry[] =>
+  ["Officer", "Army"].map(
+    (name): IEntry => ({
+      id: name.toLowerCase(),
+      title: { en: name },
+      logoUrlByWidth: createLogoUrl(name.toLocaleLowerCase()),
+    }),
+  );
+
+const createLogoUrl = (name: string): IEntry["logoUrlByWidth"] =>
+  (["48", "64", "128", "256"] as (keyof IEntry["logoUrlByWidth"])[]).reduce(
+    (acc, size) => {
+      acc[size] = `/images/entry/png/${name}${size}.png`;
+      return acc;
+    },
+    // tslint:disable-next-line:no-object-literal-type-assertion
+    {} as IEntry["logoUrlByWidth"],
+  );
