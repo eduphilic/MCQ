cfc904060181659b1a37bca3e853720fb0289091
strothj jason@jasons.io <jason@jasons.io>
2018-08-02 03:43:24 -0400

    feat(exam-taking): add subscription card content component

---
+++ src/exam-review/components/SubscriptionCardContent/SubscriptionCardContent.stories.tsx
@@ -0,0 +1,26 @@
+import { boolean } from "@storybook/addon-knobs";
+import { storiesOf } from "@storybook/react";
+import React, { cloneElement } from "react";
+
+import { CardMobileFlat } from "components/CardMobileFlat";
+import { StorybookContentCenterWrapper } from "components/storybook/StorybookContentCenterWrapper";
+import { SubscriptionCardContent } from "./SubscriptionCardContent";
+
+storiesOf("Exam Review", module).add("SubscriptionCardContent", () => {
+  const stats = {
+    Attempted: "02 Tests",
+    Remaining: "08 Tests",
+  };
+
+  const cardContent = <SubscriptionCardContent stats={stats} />;
+
+  const component = boolean("With Card", true)
+    ? cloneElement(<CardMobileFlat />, undefined, cardContent)
+    : cardContent;
+
+  return (
+    <StorybookContentCenterWrapper maxWidthPercent={50}>
+      {component}
+    </StorybookContentCenterWrapper>
+  );
+});
---
+++ src/exam-review/components/SubscriptionCardContent/SubscriptionCardContent.tsx
@@ -0,0 +1,39 @@
+import React, { SFC } from "react";
+import styled from "styled";
+
+import CardContent from "@material-ui/core/CardContent";
+
+import { Typography2 } from "components/Typography2";
+
+export type SubscriptionCardContentProps = {
+  stats: Record<string, string>;
+};
+
+export const SubscriptionCardContent: SFC<
+  SubscriptionCardContentProps
+> = props => {
+  const { stats } = props;
+
+  return (
+    <CardContent>
+      {Object.entries(stats).map(([key, value], index) => (
+        <StatRow key={`${key}-${index}`}>
+          <Typography2 variant="Subtitle2">{key}</Typography2>
+          <Typography2 variant="Subtitle2">{value}</Typography2>
+        </StatRow>
+      ))}
+    </CardContent>
+  );
+};
+
+const StatRow = styled.div`
+  display: flex;
+
+  > *:first-child {
+    width: 50%;
+  }
+
+  > *:not(:last-child) {
+    margin-bottom: ${({ theme }) => theme.spacing.unit}px;
+  }
+`;
