38f4e633166069d8e2190bfbbd121d564a6273f0
strothj jason@jasons.io <jason@jasons.io>
2018-08-02 00:55:16 -0400

    feat: add toggle for wrapping card component

--- src/exam-review/components/SubscriptionCardHeader/SubscriptionCardHeader.stories.tsx
+++ src/exam-review/components/SubscriptionCardHeader/SubscriptionCardHeader.stories.tsx
@@ -1,28 +1,32 @@
 import { boolean } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
 import { entryImages } from "common/structures/entryImages";
-import React from "react";
+import React, { cloneElement } from "react";
 
 import { CardMobileFlat } from "components/CardMobileFlat";
 import { StorybookContentCenterWrapper } from "components/storybook/StorybookContentCenterWrapper";
 import { SubscriptionCardHeader } from "./SubscriptionCardHeader";
 
-storiesOf("Exam Review", module)
-  .addDecorator(story => <CardMobileFlat>{story()}</CardMobileFlat>)
-  .addDecorator(story => (
-    <StorybookContentCenterWrapper maxWidthPercent={50}>
-      {story()}
-    </StorybookContentCenterWrapper>
-  ))
-  .add("SubscriptionCardHeader", () => {
-    const withOverline = boolean("With Overline", true);
+storiesOf("Exam Review", module).add("SubscriptionCardHeader", () => {
+  const withOverline = boolean("With Overline", true);
+  const Card = boolean("With Card", true) ? CardMobileFlat : null;
 
-    return (
-      <SubscriptionCardHeader
-        title="Soldier General Duty"
-        subheader="Validity 31st Jan 2019"
-        overline={withOverline ? "10 Mock Tests Set" : undefined}
-        imageUrl={entryImages.AirForce}
-      />
-    );
-  });
+  const component = (
+    <SubscriptionCardHeader
+      title="Soldier General Duty"
+      subheader="Validity 31st Jan 2019"
+      overline={withOverline ? "10 Mock Tests Set" : undefined}
+      imageUrl={entryImages.AirForce}
+    />
+  );
+
+  const wrappedComponent = Card
+    ? cloneElement(<Card />, undefined, component)
+    : component;
+
+  return (
+    <StorybookContentCenterWrapper maxWidthPercent={50}>
+      {wrappedComponent}
+    </StorybookContentCenterWrapper>
+  );
+});
