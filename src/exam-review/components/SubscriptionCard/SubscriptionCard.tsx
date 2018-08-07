21148c0a84c63f6d0a70759eb325d1aee2919423
strothj jason@jasons.io <jason@jasons.io>
2018-08-07 00:41:27 -0400

    feat: add hover style when an onclick handler is provided

--- src/exam-review/components/SubscriptionCard/SubscriptionCard.tsx
+++ src/exam-review/components/SubscriptionCard/SubscriptionCard.tsx
@@ -29,15 +29,40 @@ export const SubscriptionCard: SFC<SubscriptionCardProps> = props => {
   const { onClick, ...rest } = props;
 
   return (
-    <CardMobileFlat>
+    <StyledCardMobileFlat hoverable={!!onClick}>
       <ActionArea onClick={onClick}>
         <SubscriptionCardHeader {...rest} />
         <SubscriptionCardContent {...rest} />
       </ActionArea>
       <SubscriptionCardActions {...rest} />
-    </CardMobileFlat>
+    </StyledCardMobileFlat>
   );
 };
+
+const StyledCardMobileFlat = styled<{ className?: string; hoverable: boolean }>(
+  ({ className, children }) => (
+    <CardMobileFlat className={className}>{children}</CardMobileFlat>
+  ),
+)`
+  transition: ${({ theme }) =>
+    theme.transitions.create("box-shadow", {
+      duration: theme.transitions.duration.short,
+    })};
+
+  ${({ hoverable, theme }) =>
+    hoverable &&
+    `
+      &:hover {
+        box-shadow: ${theme.shadows[2]};
+      }
+
+      ${theme.breakpoints.up("md")} {
+        &:hover {
+          box-shadow: ${theme.shadows[4]};
+        }
+      }
+    `};
+`;
 
 const ActionArea = styled.div`
   ${({ onClick }) => onClick && "cursor: pointer"};
