8f5077dc9c2acee1c0c002ebaca1d75ccfabac5b
strothj jason@jasons.io <jason@jasons.io>
2018-08-06 22:25:50 -0400

    feat(subscription-management): add action to allow skipping onboarding

--- src/subscription-management/actions.ts
+++ src/subscription-management/actions.ts
@@ -7,6 +7,9 @@ export enum SubscriptionManagementAction {
   LoadPlaceholderData = "[subscription-management] Load Placeholder Data",
 
   SubscriptionAdditionSuccess = "[subscription-management] Subscription Addition Success",
+
+  // TODO: Remove this after development, it is used to skip the onboarding process.
+  SetSkipOnboardingProcess = "[subscription-management] Skip Onboarding Process",
 }
 
 export const actions = {
@@ -18,6 +21,10 @@ export const actions = {
       SubscriptionManagementAction.SubscriptionAdditionSuccess,
       subscriptions,
     ),
+
+  // TODO: Remove this after development, it is used to skip the onboarding process.
+  setSkipOnboardingProcess: () =>
+    createAction(SubscriptionManagementAction.SetSkipOnboardingProcess),
 };
 
 export type Actions = ActionsUnion<typeof actions>;
