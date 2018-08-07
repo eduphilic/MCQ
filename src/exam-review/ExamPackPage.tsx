3c37dbf05947872d309c8cc11acf7aa29a6e8f9c
strothj jason@jasons.io <jason@jasons.io>
2018-08-07 00:42:23 -0400

    feat: populate left hand side of column layout

--- src/exam-review/ExamPackPage.tsx
+++ src/exam-review/ExamPackPage.tsx
@@ -1,7 +1,11 @@
+import { entryImages } from "common/structures/entryImages";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { State } from "store";
+import styled from "styled";
 import { actions } from "./actions";
+
+import { SubscriptionCard } from "./components/SubscriptionCard";
 
 type StateProps = {
   loading: boolean;
@@ -14,7 +18,15 @@ type DispatchProps = {
 
 type Props = StateProps & DispatchProps;
 
-class ExamPackPage extends Component<Props> {
+type ExamPackPageState = {
+  selectedSubscription: number | null;
+};
+
+class ExamPackPage extends Component<Props, ExamPackPageState> {
+  state: ExamPackPageState = {
+    selectedSubscription: null,
+  };
+
   componentDidMount() {
     const { /* loading, */ loaded, fetchExamReviewData } = this.props;
 
@@ -29,8 +41,38 @@ class ExamPackPage extends Component<Props> {
 
     if (!loaded) return <div>Loading...</div>;
 
-    return <div>Exam Pack Page Placeholder</div>;
+    const subscriptions: [keyof typeof entryImages, string, boolean][] = [
+      ["Officer", "NDA/ ACC", false],
+      ["Army", "Soldier Tradesman", true],
+      ["AirForce", "Group 'X' & 'Y': Med Asst Trade", false],
+    ];
+
+    return (
+      <TwoColumnWrapper>
+        <div>
+          {subscriptions.map((subscription, index) => (
+            <SubscriptionCard
+              key={subscription[0]}
+              imageUrl={entryImages[subscription[0]]}
+              title={subscription[1]}
+              subheader="Validity 31st Jan 2019"
+              overline={subscription[2] ? "1 Free Test" : "10 Mock Tests Set"}
+              stats={{
+                Attempted: "02 Tests",
+                Remaining: "08 Tests",
+              }}
+              onClick={() => this.handleSubscriptionClick(index)}
+            />
+          ))}
+        </div>
+        <div>Column 2</div>
+      </TwoColumnWrapper>
+    );
   }
+
+  private handleSubscriptionClick = (subscriptionIndex: number) => {
+    this.setState({ selectedSubscription: subscriptionIndex });
+  };
 }
 
 const ExamPackPageContainer = connect<StateProps, DispatchProps, {}, State>(
@@ -40,3 +82,34 @@ const ExamPackPageContainer = connect<StateProps, DispatchProps, {}, State>(
   },
 )(ExamPackPage);
 export { ExamPackPageContainer as ExamPackPage };
+
+const TwoColumnWrapper = styled.div`
+  display: flex;
+
+  > * {
+    width: 100%;
+  }
+
+  > *:last-child {
+    display: none;
+  }
+
+  > div > *:not(:last-child) {
+    margin-bottom: ${({ theme }) => theme.spacing.unit * 2}px;
+  }
+
+  ${({ theme }) => theme.breakpoints.up("md")} {
+    > * {
+      width: 50%;
+    }
+
+    > *:first-child {
+      padding-right: ${({ theme }) => (theme.spacing.unit * 3) / 2}px;
+    }
+
+    > *:last-child {
+      display: block;
+      padding-left: ${({ theme }) => (theme.spacing.unit * 3) / 2}px;
+    }
+  }
+`;
