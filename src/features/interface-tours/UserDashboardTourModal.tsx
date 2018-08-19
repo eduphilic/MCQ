2546d64e5595fae61890a1500e85f148b47d3cd1
strothj jason@jasons.io <jason@jasons.io>
2018-08-19 12:34:13 -0400

    feat: add interface descriptions to dashboard tour

--- src/features/interface-tours/UserDashboardTourModal.tsx
+++ src/features/interface-tours/UserDashboardTourModal.tsx
@@ -1,6 +1,8 @@
 import React, { SFC } from "react";
 import { ArrowContainer } from "./components/ArrowContainer";
+import { ArrowTargetDescription } from "./components/ArrowTargetDescription";
 import { ArrowTargetRow } from "./components/ArrowTargetRow";
+import { ArrowTargetRowItem } from "./components/ArrowTargetRowItem";
 import { SwipeInstructions } from "./components/SwipeInstructions";
 import { TourDismissalButton } from "./components/TourDismissalButton";
 import { TourPortal } from "./components/TourPortal";
@@ -17,8 +19,47 @@ export const UserDashboardTourModal: SFC = () => {
 
           <ArrowContainer>
             <ArrowTargetRow variant="toolbar">
-              <div>Placeholder</div>
+              <div style={{ flex: 1 }} />
+
+              <ArrowTargetRowItem id="change-language-button" width={50} />
+              <ArrowTargetRowItem id="log-out-button" width={50} />
             </ArrowTargetRow>
+
+            <ArrowTargetDescription
+              id="change-language-button-description"
+              relations={[
+                {
+                  from: { anchor: "right" },
+                  to: { anchor: "bottom", id: "change-language-button" },
+                },
+              ]}
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                paddingRight: 100,
+                marginTop: 32,
+              }}
+            >
+              Change Language
+            </ArrowTargetDescription>
+
+            <ArrowTargetDescription
+              id="log-out-button-description"
+              relations={[
+                {
+                  from: { anchor: "right" },
+                  to: { anchor: "bottom", id: "log-out-button" },
+                },
+              ]}
+              style={{
+                display: "flex",
+                justifyContent: "flex-end",
+                paddingRight: 100,
+                marginTop: 32,
+              }}
+            >
+              Log Out
+            </ArrowTargetDescription>
           </ArrowContainer>
 
           <TourDismissalButton onClick={closeModal}>
