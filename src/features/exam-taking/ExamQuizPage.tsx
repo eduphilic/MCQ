ed9dbfbc5763f489698bc63c197de09391444cec
strothj jason@jasons.io <jason@jasons.io>
2018-08-19 06:58:20 -0400

    chore: rename exam taking tour feature to interface tours

--- src/features/exam-taking/ExamQuizPage.tsx
+++ src/features/exam-taking/ExamQuizPage.tsx
@@ -7,7 +7,7 @@ import { IExamMeta } from "./models/IExamMeta";
 
 import { DashboardColumnContainer } from "components/DashboardColumnContainer";
 
-import { ExamTakingTourModal } from "features/exam-taking-tour";
+import { ExamTakingTourModal } from "features/interface-tours";
 import { ExamAnswerSelect } from "./components/ExamAnswerSelect";
 import { examAnswerSelectPlaceholderProps } from "./components/ExamAnswerSelect/ExamAnswerSelect.placeholder";
 import { ExamContents } from "./components/ExamContents";
