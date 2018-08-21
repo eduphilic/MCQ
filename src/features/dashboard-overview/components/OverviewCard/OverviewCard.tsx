11fe39ca1ad75a1d2a0898c7683177db1784ead8
strothj jason@jasons.io <jason@jasons.io>
2018-08-20 21:19:55 -0400

    feat: add overview card component

---
+++ src/features/dashboard-overview/components/OverviewCard/OverviewCard.tsx
@@ -0,0 +1,43 @@
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import { CardMobileFlat } from "components/CardMobileFlat";
+import { Typography2 } from "components/Typography2";
+import React, { SFC } from "react";
+import styled from "styled";
+
+export type OverviewCardProps = {
+  title: string;
+
+  stats: Record<string, string>;
+};
+
+export const OverviewCard: SFC<OverviewCardProps> = props => {
+  const { title, stats } = props;
+
+  return (
+    <CardMobileFlat>
+      <CardHeader title={title} />
+
+      <CardContent>
+        {Object.entries(stats).map(([key, value], index) => (
+          <StatRow key={`${key}-${index}`}>
+            <Typography2 variant="Subtitle2">{key}</Typography2>
+            <Typography2 variant="Subtitle2">{value}</Typography2>
+          </StatRow>
+        ))}
+      </CardContent>
+    </CardMobileFlat>
+  );
+};
+
+const StatRow = styled.div`
+  display: flex;
+
+  > *:first-child {
+    width: 50%;
+  }
+
+  > * {
+    margin-bottom: ${({ theme }) => theme.spacing.unit}px;
+  }
+`;
