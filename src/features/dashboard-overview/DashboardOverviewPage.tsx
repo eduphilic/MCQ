eb7f3e6fa798220c818fd3a54f048a7f48085d8c
strothj jason@jasons.io <jason@jasons.io>
2018-08-20 21:20:26 -0400

    feat: add rank card to dashboard overview page

--- src/features/dashboard-overview/DashboardOverviewPage.tsx
+++ src/features/dashboard-overview/DashboardOverviewPage.tsx
@@ -1,5 +1,12 @@
 import React, { SFC } from "react";
+import { OverviewCard } from "./components/OverviewCard";
 
 export const DashboardOverviewPage: SFC = () => (
-  <div>Progress Overview Page Placeholder</div>
+  <OverviewCard
+    title="Rank"
+    stats={{
+      "Soldier GD (Army)": "123/999 (92% from 5 papers)",
+      "Artificer Apprentice (Airforce)": "123/999 (92% from 8 papers)",
+    }}
+  />
 );
