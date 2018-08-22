01bd3ab93fdb3c6c459b1e90a2ab331ee9d134dd
strothj jason@jasons.io <jason@jasons.io>
2018-08-22 07:49:28 -0400

    feat: add clickable stat value

--- src/features/dashboard-overview/components/OverviewCard/OverviewCard.tsx
+++ src/features/dashboard-overview/components/OverviewCard/OverviewCard.tsx
@@ -1,3 +1,4 @@
+import Button from "@material-ui/core/Button";
 import CardContent from "@material-ui/core/CardContent";
 import CardHeader from "@material-ui/core/CardHeader";
 import Divider from "@material-ui/core/Divider";
@@ -6,6 +7,7 @@ import Hidden from "@material-ui/core/Hidden";
 import { Typography } from "components/Typography";
 import { CardMobileFlat } from "componentsV0/CardMobileFlat";
 import React, { SFC } from "react";
+import styled from "styled";
 
 export type OverviewCardStat = {
   id: string;
@@ -17,6 +19,12 @@ export type OverviewCardProps = {
   title: string;
 
   stats: OverviewCardStat[];
+
+  /**
+   * If supplied, turns the stat value into a button. This handler is called
+   * when the button is pressed with the stat id.
+   */
+  onStatValueClick?: (id: string) => void;
 };
 
 export const OverviewCard: SFC<OverviewCardProps> = props => {
@@ -32,10 +40,10 @@ export const OverviewCard: SFC<OverviewCardProps> = props => {
             {stats.map((stat, index) => (
               <Grid key={`${stat.id}-${index}`} container spacing={16}>
                 <Grid item xs={12} md={6}>
-                  <Typography variant="Subtitle2">{stat.title}</Typography>
+                  {renderStatKey(props, stat.id)}
                 </Grid>
                 <Grid item xs={12} md={6}>
-                  <Typography variant="Subtitle2">{stat.value}</Typography>
+                  {renderStatValue(props, stat.id)}
                 </Grid>
 
                 {index < stats.length - 1 && (
@@ -53,3 +61,57 @@ export const OverviewCard: SFC<OverviewCardProps> = props => {
     </CardMobileFlat>
   );
 };
+
+/**
+ * Align the stat key with the stat value. The stat value button is 32px. If
+ * the stat value is not-clickable, do nothing special.
+ */
+const renderStatKey = (props: OverviewCardProps, id: string) => {
+  const stat = props.stats.find(s => s.id === id);
+  if (!stat) {
+    throw new Error("Could not find stat corresponding to supplied id.");
+  }
+
+  const keyNode = <Typography variant="Subtitle2">{stat.title}</Typography>;
+  if (!props.onStatValueClick) return keyNode;
+
+  return <VerticallySpacedKeyWrapper>{keyNode}</VerticallySpacedKeyWrapper>;
+};
+
+const renderStatValue = (props: OverviewCardProps, id: string) => {
+  const stat = props.stats.find(s => s.id === id);
+  if (!stat) {
+    throw new Error("Could not find stat corresponding to supplied id.");
+  }
+
+  const { onStatValueClick } = props;
+  const valueNode = <Typography variant="Subtitle2">{stat.value}</Typography>;
+  if (!onStatValueClick) return valueNode;
+
+  return (
+    <ValueButton
+      size="small"
+      classes={{ label: "label" }}
+      onClick={() => onStatValueClick(id)}
+    >
+      {valueNode}
+    </ValueButton>
+  );
+};
+
+const VerticallySpacedKeyWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  min-height: 32px;
+`;
+
+const ValueButton = styled(Button)`
+  & > .label {
+    text-transform: none;
+  }
+
+  * {
+    color: #2d9cdb !important;
+  }
+`;
