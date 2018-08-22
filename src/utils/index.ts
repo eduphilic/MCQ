850a8d04ff3c4446ddfa565f67dc8ccb1ea867c8
strothj jason@jasons.io <jason@jasons.io>
2018-08-22 09:09:07 -0400

    chore: remove unused utility

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,2 @@
 export * from "./action-helpers";
 export * from "./reducer-helpers";
-export * from "./store-errors";
--- src/utils/store-errors.ts
+++
@@ -1,4 +0,0 @@
-export const createStoreNullError = (field: string) =>
-  new Error(
-    `"${field}" was null in the store but was expected to contain a value.`,
-  );
