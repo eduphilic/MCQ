c6ea2a289eac6669be84d226e9d58c93105b799f
strothj jason@jasons.io <jason@jasons.io>
2018-08-24 05:43:09 -0400

    fix: lint error

--- src/componentsV0/AttemptButton/index.stories.tsx
+++ src/componentsV0/AttemptButton/index.stories.tsx
@@ -2,4 +2,6 @@ import { storiesOf } from "@storybook/react";
 import React from "react";
 import { AttemptButton } from ".";
 
-storiesOf("Components V0", module).add("AttemptButton", () => <AttemptButton />);
+storiesOf("Components V0", module).add("AttemptButton", () => (
+  <AttemptButton />
+));
