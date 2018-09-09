73a3e71db7b49ee4ddb42dc9c94a43a8d979fa72
strothj jason@jasons.io <jason@jasons.io>
2018-09-08 21:03:22 -0400

    feat: remove stat sfrom free exam card

--- src/features/dashboard-overview/components/FreeExamCard/FreeExamCard.tsx
+++ src/features/dashboard-overview/components/FreeExamCard/FreeExamCard.tsx
@@ -1,24 +1,24 @@
 import { entryImages } from "common/structures/entryImages";
 import { Card } from "components/Card";
 import { CardActionArea } from "components/CardActionArea";
-import {
-  CardDualColumnsTextContent,
-  CardDualColumnsTextContentRow,
-} from "components/CardDualColumnsTextContent";
+// import {
+//   CardDualColumnsTextContent,
+//   CardDualColumnsTextContentRow,
+// } from "components/CardDualColumnsTextContent";
 import { CardHeader } from "components/CardHeader";
-import { Typography } from "components/Typography";
+// import { Typography } from "components/Typography";
 import React from "react";
 import styled from "styled";
 
 export const FreeExamCard = () => {
-  const rows: CardDualColumnsTextContentRow[] = [
-    { id: "attempted", leftNode: "Attempted", rightNode: "02 Tests" },
-    { id: "remaining", leftNode: "Remaining", rightNode: "08 Tests" },
-  ].map(row => ({
-    ...row,
-    leftNode: <Typography variant="Subtitle2">{row.leftNode}</Typography>,
-    rightNode: <Typography variant="Subtitle2">{row.rightNode}</Typography>,
-  }));
+  // const rows: CardDualColumnsTextContentRow[] = [
+  //   { id: "attempted", leftNode: "Attempted", rightNode: "02 Tests" },
+  //   { id: "remaining", leftNode: "Remaining", rightNode: "08 Tests" },
+  // ].map(row => ({
+  //   ...row,
+  //   leftNode: <Typography variant="Subtitle2">{row.leftNode}</Typography>,
+  //   rightNode: <Typography variant="Subtitle2">{row.rightNode}</Typography>,
+  // }));
 
   return (
     <StyledCard hoverable>
@@ -29,7 +29,7 @@ export const FreeExamCard = () => {
           overline="1 Free Test"
           imageUrl={entryImages.Army}
         />
-        <CardDualColumnsTextContent rows={rows} />
+        {/* <CardDualColumnsTextContent rows={rows} /> */}
       </CardActionArea>
     </StyledCard>
   );
