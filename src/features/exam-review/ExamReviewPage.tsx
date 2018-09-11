b74a22d0be519edb1c81093d8add0daadc1869b3
strothj jason@jasons.io <jason@jasons.io>
2018-09-11 03:58:05 -0400

    feat: start exam review page

--- src/features/exam-review/ExamReviewPage.tsx
+++ src/features/exam-review/ExamReviewPage.tsx
@@ -1,3 +1,13 @@
+import { ExamTemplate } from "features/exam-taking";
 import React from "react";
 
-export const ExamReviewPage = () => <div>Exam Revise Page</div>;
+const paneKeyNodeMap = Array.from({ length: 15 }, (_, index) => ({
+  key: `pane-${index}`,
+  node: <div>Pane Node</div>,
+}));
+
+export const ExamReviewPage = () => (
+  <ExamTemplate featureKey="examReview" paneKeyNodeMap={paneKeyNodeMap}>
+    {paneKeyNodeMap[0].node}
+  </ExamTemplate>
+);
