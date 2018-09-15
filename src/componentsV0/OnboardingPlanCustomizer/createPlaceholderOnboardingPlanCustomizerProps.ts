2536976cc553e789ee8d83870bf7d4eeeff0b600
strothj jason@jasons.io <jason@jasons.io>
2018-09-15 08:21:18 -0400

    chore: remove unused OnboardingPlanCustomizer component

--- src/componentsV0/OnboardingPlanCustomizer/createPlaceholderOnboardingPlanCustomizerProps.ts
+++
@@ -1,6 +0,0 @@
-import { createPlaceholderQuantitySliderProps } from "componentsV0/QuantitySlider/createPlaceholderQuantitySliderProps";
-import { OnboardingPlanCustomizerProps } from ".";
-
-export const createPlaceholderOnboardingPlanCustomizerProps = (): OnboardingPlanCustomizerProps => ({
-  ...createPlaceholderQuantitySliderProps(),
-});
