0f208cc07a4e03fe02bf7103b2bfa396b3368baa
strothj jason@jasons.io <jason@jasons.io>
2018-09-15 08:22:28 -0400

    chore: remove unused QuantitySlider component

--- src/componentsV0/QuantitySlider/createPlaceholderQuantitySliderProps.ts
+++
@@ -1,8 +0,0 @@
-import { QuantitySliderProps } from ".";
-
-export const createPlaceholderQuantitySliderProps = (): QuantitySliderProps => ({
-  itemDescription: "Exams",
-  max: 100,
-  min: 20,
-  step: 5,
-});
