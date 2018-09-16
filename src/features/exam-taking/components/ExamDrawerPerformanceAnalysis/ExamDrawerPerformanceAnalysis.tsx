0e8a6d5027bbbbb0c316a08d390b0685f9eab24e
strothj jason@jasons.io <jason@jasons.io>
2018-09-16 09:06:44 -0400

    feat: add test result text and color

--- src/features/exam-taking/components/ExamDrawerPerformanceAnalysis/ExamDrawerPerformanceAnalysis.tsx
+++ src/features/exam-taking/components/ExamDrawerPerformanceAnalysis/ExamDrawerPerformanceAnalysis.tsx
@@ -1,6 +1,6 @@
 import { Typography } from "componentsV0/Typography";
 import { examDrawerInfoCardBackground } from "css";
-import React, { SFC } from "react";
+import React, { Fragment, ReactNode, SFC } from "react";
 import styled from "styled";
 
 export type ExamDrawerPerformanceAnalysisProps = {};
@@ -13,40 +13,36 @@ export const ExamDrawerPerformanceAnalysis: SFC<
   return (
     <div>
       <TitleWrapper>
-        <Typography variant="examDrawerTitle">
+        <SectionText bold bottomMargin>
           Army Soldier GD Test 1
-        </Typography>
+        </SectionText>
       </TitleWrapper>
 
-      <Wrapper>
-        <Typography
-          variant="examDrawerTitle"
-          muiTypographyProps={{ paragraph: true }}
-        >
+      <SectionWrapper>
+        <SectionText bold bottomMargin>
           Performance Analysis
-        </Typography>
+        </SectionText>
 
-        <Typography
-          variant="examDrawerSubtitle"
-          muiTypographyProps={{ paragraph: true }}
-        >
-          Total Questions – 50
-          <br />
-          Not Answered – 10
-          <br />
-          Correctly Answered – 30
-          <br />
-          Wrong Answers – 10
-          <br />
-        </Typography>
-      </Wrapper>
+        {[
+          "Total Questions – 50",
+          "Not Answered – 10",
+          "Correctly Answered – 30",
+          "Wrong Answers – 10",
+        ].map(text => (
+          <SectionText key={text}>{text}</SectionText>
+        ))}
+      </SectionWrapper>
+
+      <SectionWrapper>
+        <SectionText>Total Attempts - 2345</SectionText>
+        <SectionText>Total Passed - 1234</SectionText>
+        <SectionText bold statColor="green">
+          Your Result - Pass
+        </SectionText>
+      </SectionWrapper>
     </div>
   );
 };
-
-const Wrapper = styled.div`
-  padding: 16px;
-`;
 
 const TitleWrapper = styled.div`
   padding: 8px;
@@ -56,5 +52,59 @@ const TitleWrapper = styled.div`
   > *:first-child {
     margin: 8px 0;
     margin-bottom: 16px;
+  }
+`;
+
+const SectionWrapper = styled.div`
+  padding: 16px;
+`;
+
+type SectionTextProps = {
+  className?: string;
+  bold?: boolean;
+  bottomMargin?: boolean;
+  statColor?: "none" | "green" | "red";
+};
+
+const SectionText = styled<SectionTextProps>(props => {
+  const { children, className, bold, bottomMargin, statColor = "none" } = props;
+
+  if (typeof children !== "string") {
+    throw new Error('Expected string for field "children".');
+  }
+
+  let text: ReactNode = children;
+
+  if (statColor !== "none") {
+    const result = /.*- */.exec(children);
+    if (!result) throw new Error("Incorrect format for text.");
+
+    const description = result[0];
+    const stat = children.slice(description.length);
+
+    text = [
+      <Fragment key="description">{description}</Fragment>,
+      <Fragment key="stat">
+        <span className={statColor}>{stat}</span>
+      </Fragment>,
+    ];
+  }
+
+  return (
+    <Typography
+      className={className}
+      variant={bold ? "examDrawerTitle" : "examDrawerSubtitle"}
+      muiTypographyProps={{ paragraph: bottomMargin }}
+    >
+      {text}
+    </Typography>
+  );
+})`
+  .green {
+    color: #00b050;
+  }
+
+  .red {
+    color: #ff0000;
   }
 `;
