275dc35350b8952e79e506852cf553c16d40a29c
strothj jason@jasons.io <jason@jasons.io>
2018-09-15 20:19:33 -0400

    feat: add initial contents to exam review page

--- src/features/exam-review/ExamReviewPage.tsx
+++ src/features/exam-review/ExamReviewPage.tsx
@@ -1,9 +1,35 @@
-import { ExamTemplate } from "features/exam-taking";
+import {
+  ExamAnswerSelect,
+  examAnswerSelectPlaceholderProps,
+  ExamContents,
+  examContentsPlaceholderProps,
+  ExamHeader,
+  examHeaderPlaceholderProps,
+  ExamLayout,
+  ExamTemplate,
+} from "features/exam-taking";
 import React from "react";
 
 const paneKeyNodeMap = Array.from({ length: 15 }, (_, index) => ({
   key: `pane-${index}`,
-  node: <div>Pane Node</div>,
+  node: (
+    <ExamLayout
+      headerNode={
+        <ExamHeader
+          questionNumber={index + 1}
+          {...examHeaderPlaceholderProps}
+        />
+      }
+      contentsNode={
+        <ExamContents
+          {...examContentsPlaceholderProps}
+          answerNode={
+            <ExamAnswerSelect {...examAnswerSelectPlaceholderProps} />
+          }
+        />
+      }
+    />
+  ),
 }));
 
 export const ExamReviewPage = () => (
