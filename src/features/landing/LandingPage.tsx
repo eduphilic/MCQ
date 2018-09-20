02cf046881474a2328a813746bdc496aec77a8e0
strothj jason@jasons.io <jason@jasons.io>
2018-09-20 13:38:29 -0400

    feat(Landing): pass YouTube videos component to landing template

--- src/features/landing/LandingPage.tsx
+++ src/features/landing/LandingPage.tsx
@@ -4,12 +4,14 @@ import { PageFooter } from "componentsV0/PageFooter";
 import { Hero } from "./components/Hero";
 import { IndexCards } from "./components/IndexCards";
 import { LandingTemplate } from "./components/LandingTemplate";
+import { YouTubeVideos } from "./components/YouTubeVideos";
 
 export const LandingPage: SFC<{}> = () => {
   return (
     <LandingTemplate
       heroNode={<Hero />}
       testCardNode={<IndexCards />}
+      youTubeVideosNode={<YouTubeVideos />}
       footerNode={<PageFooter />}
     />
   );
