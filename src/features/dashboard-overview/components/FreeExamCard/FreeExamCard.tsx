0969111a09f523fe2f6051169fe23039553480f0
strothj jason@jasons.io <jason@jasons.io>
2018-09-23 12:10:18 -0400

    feat: add attempt button

--- src/features/dashboard-overview/components/FreeExamCard/FreeExamCard.tsx
+++ src/features/dashboard-overview/components/FreeExamCard/FreeExamCard.tsx
@@ -1,4 +1,8 @@
+import CardActions from "@material-ui/core/CardActions";
+import Hidden from "@material-ui/core/Hidden";
 import { entryImages } from "common/structures/entryImages";
+// import { Typography } from "components/Typography";
+import { Button } from "components/Button";
 import { Card } from "components/Card";
 import { CardActionArea } from "components/CardActionArea";
 // import {
@@ -6,9 +10,8 @@ import { CardActionArea } from "components/CardActionArea";
 //   CardDualColumnsTextContentRow,
 // } from "components/CardDualColumnsTextContent";
 import { CardHeader } from "components/CardHeader";
-// import { Typography } from "components/Typography";
 import React from "react";
-import styled from "styled";
+import styled, { css } from "styled";
 
 export const FreeExamCard = () => {
   // const rows: CardDualColumnsTextContentRow[] = [
@@ -20,21 +23,59 @@ export const FreeExamCard = () => {
   //   rightNode: <Typography variant="Subtitle2">{row.rightNode}</Typography>,
   // }));
 
+  const onCardClick = () => alert("Start free exam");
+
   return (
     <StyledCard hoverable>
-      <CardActionArea onClick={() => alert("Start free exam")}>
-        <CardHeader
-          title="Soldier Tradesman"
-          subheader="Validity 31st Jan 2019"
-          overline="1 Free Test"
-          imageUrl={entryImages.Army}
-        />
-        {/* <CardDualColumnsTextContent rows={rows} /> */}
+      <CardActionArea onClick={onCardClick}>
+        <div style={{ position: "relative" }}>
+          <CardHeader
+            title="Soldier Tradesman"
+            subheader="Validity 31st Jan 2019"
+            overline="1 Free Test"
+            imageUrl={entryImages.Army}
+          />
+          <Hidden smDown>
+            <AttemptButton position="right" />
+          </Hidden>
+        </div>
       </CardActionArea>
+      <Hidden mdUp>
+        <CardActions>
+          <AttemptButton position="bottom" onClick={onCardClick} />
+        </CardActions>
+      </Hidden>
     </StyledCard>
   );
 };
 
 const StyledCard = styled(Card)`
   background-color: #ecd100;
+`;
+
+const AttemptButton = styled<{
+  className?: string;
+  onClick?: () => void;
+  position: "right" | "bottom";
+}>(({ className, onClick }) => (
+  <Button
+    className={className}
+    variant="contained"
+    color="primary"
+    onClick={onClick}
+  >
+    Attempt
+  </Button>
+))`
+  ${({ position }) =>
+    position === "right"
+      ? css`
+          position: absolute;
+          right: 24px;
+          top: 50%;
+          transform: translateY(-50%);
+        `
+      : css`
+          margin-left: auto;
+        `};
 `;
