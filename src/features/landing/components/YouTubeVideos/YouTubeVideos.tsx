6b14dbf8c1d11d15780f7c12a228bcc8498fd06f
strothj jason@jasons.io <jason@jasons.io>
2018-09-23 08:56:27 -0400

    feat: add initial youtube video boxes

--- src/features/landing/components/YouTubeVideos/YouTubeVideos.tsx
+++ src/features/landing/components/YouTubeVideos/YouTubeVideos.tsx
@@ -1,14 +1,15 @@
 import ButtonBase from "@material-ui/core/ButtonBase";
 import { Typography } from "components/Typography";
+import { ContentCenterWrapper } from "componentsV0/ContentCenterWrapper";
 import React, { SFC } from "react";
 import Loadable from "react-loadable";
-import styled from "styled";
+import styled, { css } from "styled";
 
 import { fetchVideos, YouTubeVideoList } from "./client";
 import YouTubeSubscribeOptimizedPng from "./YouTubeSubscribeOptimized.png";
 
-/* tslint:disable-next-line:no-console */
-// console.log("googleYouTubeApiKey", googleYouTubeApiKey);
+const youtubeChannelUrl =
+  "https://www.youtube.com/channel/UCegkxRhEtHGlE60RAo4ZwSg";
 
 export type YouTubeVideosProps = {
   videos: YouTubeVideoList;
@@ -20,19 +21,52 @@ const YouTubeVideos: SFC<YouTubeVideosProps> = props => {
   /* tslint:disable-next-line:no-console */
   console.log("videos", videos);
 
+  const categories: {
+    title: string;
+    videos: YouTubeVideoList;
+  }[] = [
+    { title: "Army", videos: [] },
+    { title: "Air Force", videos: [] },
+    // { title: "Navy", videos: [] },
+  ];
+
+  // Distribute 4 videos per category.
+  // TODO: Replace with logic which pulls from YouTube channels.
+  let nextCategoryIndex = 0;
+  for (const video of videos.slice(0, categories.length * 4)) {
+    categories[nextCategoryIndex].videos.push(video);
+    nextCategoryIndex += 1;
+    if (nextCategoryIndex === categories.length) nextCategoryIndex = 0;
+  }
+
   return (
     <>
-      <div>
+      <Wrapper>
         <HeaderWrapper>
           <Header>Our Information Videos on Defense Jobs</Header>
           <YouTubeSubscriptionButton />
         </HeaderWrapper>
-        {videos.map(video => (
+
+        <ContentCenterWrapper>
+          {categories.map(category => (
+            <div key={category.title}>
+              <p>{category.title}</p>
+
+              <div style={{ display: "flex" }}>
+                {category.videos.map(({ contentDetails: { videoId } }) => (
+                  <YouTubeVideoIframe key={videoId} videoId={videoId} />
+                ))}
+              </div>
+            </div>
+          ))}
+        </ContentCenterWrapper>
+
+        {/* {videos.map(video => (
           <p key={video.contentDetails.videoId}>
             {video.snippet.position + 1}. {video.snippet.title}
           </p>
-        ))}
-      </div>
+        ))} */}
+      </Wrapper>
     </>
   );
 };
@@ -46,6 +80,10 @@ const LoadableYouTubeVideos = Loadable({
 });
 
 export { LoadableYouTubeVideos as YouTubeVideos };
+
+const Wrapper = styled.div`
+  margin-bottom: 16px;
+`;
 
 const HeaderWrapper = styled.div`
   display: flex;
@@ -66,7 +104,7 @@ const YouTubeSubscriptionButton = styled<{ className?: string }>(
     <ButtonBase
       className={className}
       component="a"
-      href="https://www.youtube.com/channel/UCegkxRhEtHGlE60RAo4ZwSg"
+      href={youtubeChannelUrl}
       target="_blank"
     >
       <span className="background-image" />
@@ -93,5 +131,37 @@ const YouTubeSubscriptionButton = styled<{ className?: string }>(
     background-image: url(${YouTubeSubscribeOptimizedPng});
     background-size: cover;
     background-position: center;
+  }
+`;
+
+const videoRowSplitCount = 5;
+
+const videoDimensions = css`
+  width: calc(100vw / ${videoRowSplitCount} - 32px);
+  height: calc(100vw / ${videoRowSplitCount} - 32px);
+  max-width: calc(1280px / ${videoRowSplitCount} - 32px);
+  max-height: calc(1280px / ${videoRowSplitCount} - 32px);
+`;
+
+const YouTubeVideoIframe = styled<{ className?: string; videoId: string }>(
+  ({ className, videoId }) => (
+    <iframe
+      className={className}
+      src={`https://www.youtube.com/embed/${videoId}`}
+      frameBorder={0}
+      allowFullScreen
+    />
+  ),
+)`
+  ${videoDimensions};
+
+  margin: 16px;
+
+  &:first-child {
+    margin-left: 0;
+  }
+
+  &:last-child {
+    margin-right: 0;
   }
 `;
