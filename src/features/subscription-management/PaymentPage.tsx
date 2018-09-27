2ca3034b9e7d8345eb3c7ec6d67421b54f92129a
strothj jason@jasons.io <jason@jasons.io>
2018-09-26 20:04:06 -0400

    feat: add payment page initial content

--- src/features/subscription-management/PaymentPage.tsx
+++ src/features/subscription-management/PaymentPage.tsx
@@ -1,3 +1,65 @@
+import RemoveCircle from "@material-ui/icons/RemoveCircle";
+import { Button, ButtonProps } from "components/Button";
+import { Card } from "components/Card";
+import { CardContent } from "components/CardContent";
+import { CardHeader } from "components/CardHeader";
+import { Typography } from "components/Typography";
 import React from "react";
+import styled from "styled";
 
-export const PaymentPage = () => <div>Payment Page</div>;
+export const PaymentPage = () => (
+  <>
+    <Card>
+      <CardHeader title="Order Summary" subheader="Order No: 17238351" />
+      <CardContent>
+        <Typography variant="H6" paragraph>
+          Your Items
+        </Typography>
+
+        {[
+          "Soldier GD 30 Mock Tests",
+          "Soldier Tech 30 Mock Tests",
+          "Soldier RT 30 Mock Tests",
+        ].map(categoryTitle => (
+          <div key={categoryTitle}>
+            <CategoryButton onClick={() => alert("Update basket")}>
+              {categoryTitle}
+            </CategoryButton>
+          </div>
+        ))}
+      </CardContent>
+      <CardContent>
+        <Typography variant="H6" paragraph>
+          Offers &amp; Discount
+        </Typography>
+      </CardContent>
+    </Card>
+
+    <Card>
+      <CardHeader title="Payment Processor" />
+    </Card>
+  </>
+);
+
+const CategoryButton = styled<
+  Omit<ButtonProps, "innerRef"> & {
+    children: string;
+  }
+>(({ children, ...rest }) => (
+  <Button {...rest}>
+    {children}
+    <span className="icon-wrapper">
+      <RemoveCircle />
+    </span>
+  </Button>
+))`
+  justify-content: flex-start;
+  width: 300px;
+
+  .icon-wrapper {
+    flex: 1;
+    display: inline-flex;
+    justify-content: flex-end;
+    color: ${({ theme }) => theme.palette.error.main};
+  }
+`;
