f27cb9238bfd7ee006a4b76ef91b6f26d8d32518
strothj jason@jasons.io <jason@jasons.io>
2018-10-09 04:34:54 -0400

    chore: use typescript compatible build of react-scripts

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,12 @@ import { hydrate, render } from "react-dom";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root")!;
+// export interface TypeScriptComponentProps<T extends string = string> {
+//   message: T;
+// }
+
+const rootElement = document.getElementById("root");
+// @ts-ignore
 const renderFunc = rootElement.hasChildNodes() ? hydrate : render;
 
 renderFunc(<App />, document.getElementById("root"));
