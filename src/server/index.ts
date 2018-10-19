f946d12cb663d937f30a74d179bcf594e90690a0
strothj jason@jasons.io <jason@jasons.io>
2018-10-19 19:25:00 -0400

    feat: serve static files from development server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,15 @@
 import * as functions from "firebase-functions";
 import Koa from "koa";
+import serve from "koa-static";
 import { applyApolloServerMiddleware } from "./applyApolloServerMiddleware";
 import { middlewareRenderApp } from "./middlewareRenderApp";
 
 const app = new Koa();
 
 applyApolloServerMiddleware(app);
+if (process.env.NODE_ENV === "development") {
+  app.use(serve(process.env.RAZZLE_PUBLIC_DIR!));
+}
 app.use(middlewareRenderApp);
 
 export const main = functions.https.onRequest(app.callback());
