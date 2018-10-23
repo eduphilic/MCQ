e53377a406b98a84c3f44b5f98d9c49fb02198f6
strothj jason@jasons.io <jason@jasons.io>
2018-10-23 01:54:22 -0400

    feat: move hero section into dedicated component

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,6 @@
 export * from "./HtmlConfig";
+export * from "./IndexAboutImage";
+export * from "./IndexConfig";
 export * from "./Language";
 export * from "./Localization";
 export * from "./LocalizedString";
