f8f9c656f370a654b7ecfc894382be97181e9b61
strothj jason@jasons.io <jason@jasons.io>
2018-10-24 15:11:57 -0400

    feat: remove boost client from app client

--- src/client.tsx
+++ src/client.tsx
@@ -1,24 +1,53 @@
 import { createGenerateClassName, jssPreset } from "@material-ui/core/styles";
-import ApolloClient, { InMemoryCache } from "apollo-boost";
 import { create } from "jss";
 import lzString from "lz-string";
 import React, { Component } from "react";
-import { ApolloProvider } from "react-apollo";
 import { hydrate } from "react-dom";
 import JssProvider from "react-jss/lib/JssProvider";
 import { App } from "./app";
+
+import { InMemoryCache } from "apollo-cache-inmemory";
+import ApolloClient from "apollo-client";
+import { ApolloLink } from "apollo-link";
+import { onError } from "apollo-link-error";
+import { HttpLink } from "apollo-link-http";
+import { withClientState } from "apollo-link-state";
+import { ApolloProvider } from "react-apollo";
+import { LocalizationGraphQLLink } from "./features/localization/LocalizationGraphQLLink";
 import { resolvers } from "./store/resolvers-client";
 
 const initialCacheState = JSON.parse(
   lzString.decompressFromUTF16(window.__STATE__),
 );
 
+const cache = new InMemoryCache().restore(initialCacheState);
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+  }
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const stateLink = withClientState({
+  cache,
+  resolvers,
+});
+
+const httpLink = new HttpLink({ uri: "/graphql" });
+
 const client = new ApolloClient({
-  uri: "/graphql",
-  cache: new InMemoryCache().restore(initialCacheState),
-  clientState: {
-    resolvers,
-  },
+  link: ApolloLink.from([
+    errorLink,
+    stateLink,
+    new LocalizationGraphQLLink(),
+    httpLink,
+  ]),
+  cache,
 });
 
 // https://material-ui.com/customization/css-in-js/#other-html-element
