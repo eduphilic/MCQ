899e2d517ed8135ee1d4072c66e2305653725851
strothj jason@jasons.io <jason@jasons.io>
2018-10-24 15:11:09 -0400

    feat: remove unneeded localization provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import { Router } from "@reach/router";
 import React from "react";
-import { LocalizationProvider } from "./features/localization";
 import { AdminLoginPage, RootIndexPage } from "./pages";
 import { LightTheme, ThemeBaseline } from "./styled";
 
 export const App = () => (
   <ThemeBaseline>
     <LightTheme>
-      <LocalizationProvider>
-        <Router>
-          <RootIndexPage path="/" />
-          <AdminLoginPage path="/admin/login" />
-        </Router>
-      </LocalizationProvider>
+      <Router>
+        <RootIndexPage path="/" />
+        <AdminLoginPage path="/admin/login" />
+      </Router>
     </LightTheme>
   </ThemeBaseline>
 );
