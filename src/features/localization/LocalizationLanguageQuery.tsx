1872d17af1567fde9c4e98965c2ff4dc55a6a874
strothj jason@jasons.io <jason@jasons.io>
2018-10-25 16:26:08 -0400

    fix: wait for query to resolve before passing along current localization

--- src/features/localization/LocalizationLanguageQuery.tsx
+++ src/features/localization/LocalizationLanguageQuery.tsx
@@ -1,7 +1,7 @@
 import gql from "graphql-tag";
 import React, { ConsumerProps } from "react";
-import { Query } from "react-apollo";
 import { ClientState } from "../../ClientState";
+import { QueryWithLoading } from "../../components/QueryWithLoading";
 import { LocalizationSupportedLanguages } from "../../models";
 
 const GET_LOCALIZATION_LANGUAGE = gql`
@@ -17,7 +17,7 @@ type Response = Pick<ClientState, "localization">;
 export const LocalizationLanguageQuery = ({
   children,
 }: ConsumerProps<LocalizationSupportedLanguages>) => (
-  <Query<Response> query={GET_LOCALIZATION_LANGUAGE}>
+  <QueryWithLoading<Response> query={GET_LOCALIZATION_LANGUAGE}>
     {({ data }) => children(data!.localization.language)}
-  </Query>
+  </QueryWithLoading>
 );
