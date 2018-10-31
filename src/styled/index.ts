809fcfab622bc731af1018ac765f2d5eb3434ba1
strothj jason@jasons.io <jason@jasons.io>
2018-10-31 06:31:28 -0400

    feat: add styled components version  of the mui mixins

--- src/styled/index.ts
+++ src/styled/index.ts
@@ -1,6 +1,7 @@
 export * from "./themeProviders";
 export * from "./styledComponents";
 export * from "./ThemeBaseline";
+export * from "./mixins";
 
 import { Theme as BaseTheme } from "./themes";
 export type Theme = BaseTheme;
