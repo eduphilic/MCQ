ed55d5871fe3f217974c529001a283fd72053465
strothj jason@jasons.io <jason@jasons.io>
2018-11-01 09:21:35 -0400

    feat: add initial form item to hero card

--- src/pages/AdminIndexManagerPage.tsx
+++ src/pages/AdminIndexManagerPage.tsx
@@ -1,11 +1,97 @@
+import {
+  CardActions,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { Formik } from "formik";
+import gql from "graphql-tag";
 import React from "react";
+import { Button } from "../components/Button";
+import { Card } from "../components/Card";
+import { ContentCenterWrapper } from "../components/ContentCenterWrapper";
+import { QueryWithLoading } from "../components/QueryWithLoading";
+import { AdminCardHeader } from "../features/admin";
 import { DashboardLayout } from "../layouts/DashboardLayout";
+import { LocalizedString } from "../models";
 
 function AdminIndexManagerPage() {
   return (
     <DashboardLayout>
-      <div>AdminIndexManagerPage</div>
+      <ContentCenterWrapper>
+        <Grid container spacing={16}>
+          <Grid item xs={12}>
+            <HeroCard />
+          </Grid>
+        </Grid>
+      </ContentCenterWrapper>
     </DashboardLayout>
+  );
+}
+
+const GET_HERO_CONFIG = gql`
+  query GetHeroConfig {
+    indexPageConfig {
+      heroPrimaryText
+    }
+  }
+`;
+
+type HeroConfig = {
+  indexPageConfig: {
+    heroPrimaryText: LocalizedString;
+  };
+};
+
+function HeroCard() {
+  return (
+    <Card>
+      <AdminCardHeader title="Hero" />
+      <QueryWithLoading<HeroConfig> query={GET_HERO_CONFIG}>
+        {({ data }) => (
+          <Formik<HeroConfig> initialValues={data!} onSubmit={() => {}}>
+            {form => (
+              <>
+                <CardContent>
+                  <Typography variant="subtitle2" paragraph>
+                    Hero Text
+                  </Typography>
+                  <Grid container spacing={16}>
+                    <Grid item xs={12} md={6}>
+                      <TextField
+                        name="indexPageConfig.heroPrimaryText.en"
+                        label="Hero Primary Text (English)"
+                        fullWidth
+                        onChange={form.handleChange}
+                        onBlur={form.handleBlur}
+                        value={form.values.indexPageConfig.heroPrimaryText.en}
+                      />
+                    </Grid>
+                  </Grid>
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="small"
+                    color="primary"
+                    disabled={!form.touched.indexPageConfig}
+                  >
+                    Reset
+                  </Button>
+                  <Button
+                    size="small"
+                    color="primary"
+                    disabled={!form.touched.indexPageConfig}
+                  >
+                    Submit
+                  </Button>
+                </CardActions>
+              </>
+            )}
+          </Formik>
+        )}
+      </QueryWithLoading>
+    </Card>
   );
 }
 
