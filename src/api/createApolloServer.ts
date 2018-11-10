06677a3d0c79e6f4ebbe33a7a1f11a913b7e4c84
strothj jason@jasons.io <jason@jasons.io>
2018-11-09 21:49:37 -0500

    feat: enable cookies in graphql playground

--- src/api/createApolloServer.ts
+++ src/api/createApolloServer.ts
@@ -1,3 +1,4 @@
+import { ISettings } from "@apollographql/graphql-playground-html/dist/render-playground-page";
 import { ContextFunction } from "apollo-server-core";
 import { ApolloServer, makeExecutableSchema } from "apollo-server-koa";
 import { createApolloTypeDefs } from "./createApolloTypeDefs";
@@ -10,5 +11,11 @@ export function createApolloServer(contextFactory: ContextFunction<Context>) {
   return new ApolloServer({
     schema: makeExecutableSchema({ typeDefs, resolvers }),
     context: contextFactory,
+    playground:
+      process.env.NODE_ENV === "development"
+        ? {
+            settings: { "request.credentials": "same-origin" } as ISettings,
+          }
+        : false,
   });
 }
