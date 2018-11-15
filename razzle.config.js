528f46cb3a331112ca90c20bb131d8016408e64f
strothj <jason@jasons.io>
2018-11-15 03:56:31 -0500

    chore: move firebase environmental variable retrieval to razzle config

--- razzle.config.js
+++ razzle.config.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const childProcess = require("child_process");
 const rimraf = require("rimraf");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
@@ -41,6 +42,8 @@ module.exports = {
       );
 
       if (dev) {
+        retrieveProductionEnvironmentalVariables();
+
         config.plugins.push(
           new WebpackShellPlugin({
             onBuildEnd: ["yarn firebase:start"],
@@ -149,4 +152,54 @@ class CustomPostBuildPlugin {
       );
     });
   }
+}
+
+/**
+ * The Firebase Functions development emulator does not have access to
+ * configured production environmental variables. To work around this we
+ * retrieve using the command documented below:
+ *
+ * @see https://firebase.google.com/docs/functions/local-emulator#install_and_configure_the_cloud_functions_shell
+ */
+function retrieveProductionEnvironmentalVariables() {
+  /* tslint:disable-next-line:no-console */
+  console.log("Retrieving Firebase environmental variables...");
+
+  const stdout = childProcess.execSync("yarn firebase functions:config:get", {
+    cwd: path.resolve(__dirname),
+    env: process.env,
+    encoding: "utf8",
+  });
+
+  // Trim any text surrounding the stdout from the executed command. We are only
+  // interested in the printed out JSON.
+  let foundStart = false;
+  let foundEnd = false;
+  const firebaseEnvironmentalVariablesJson = stdout
+    .split("\n")
+    .filter(l => {
+      if (!foundStart && l !== "{") return false;
+      if (!foundStart && l === "{") {
+        foundStart = true;
+        return true;
+      }
+      if (!foundEnd && l === "}") {
+        foundEnd = true;
+        return true;
+      }
+      if (foundEnd) return false;
+      return true;
+    })
+    .join("\n");
+
+  // "functions.config(...)" is interested in a file called
+  // ".runtimeconfig.json" which includes the environmental variables.
+  fs.writeFileSync(
+    path.resolve(__dirname, ".runtimeconfig.json"),
+    firebaseEnvironmentalVariablesJson,
+    "utf8",
+  );
+
+  /* tslint:disable-next-line:no-console */
+  console.log("...done.");
 }
--- src/server/getInitializedFirebaseEnvironment.ts
+++ src/server/getInitializedFirebaseEnvironment.ts
@@ -3,7 +3,6 @@ import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 import "firebase-functions";
 
-import childProcess from "child_process";
 import fs from "fs";
 import path from "path";
 
@@ -21,8 +20,6 @@ let serviceAccountCredentials: {
  */
 export function getInitializedFirebaseEnvironment() {
   if (!initialized) {
-    retrieveProductionEnvironmentalVariables();
-
     // Load service account credentials to allow use of RemoteConfig API.
     try {
       serviceAccountCredentials = JSON.parse(
@@ -52,55 +49,4 @@ export function getInitializedFirebaseEnvironment() {
   }
 
   return { admin, functions, firestore: firestore!, serviceAccountCredentials };
-}
-
-/**
- * The Firebase Functions development emulator does not have access to
- * configured production environmental variables. To work around this we
- * retrieve using the command documented below:
- *
- * @see https://firebase.google.com/docs/functions/local-emulator#install_and_configure_the_cloud_functions_shell
- */
-function retrieveProductionEnvironmentalVariables() {
-  if (process.env.NODE_ENV === "production") return;
-  /* tslint:disable-next-line:no-console */
-  console.log("Retrieving Firebase environmental variables...");
-
-  const stdout = childProcess.execSync("yarn firebase functions:config:get", {
-    cwd: path.resolve(__dirname, ".."),
-    env: process.env,
-    encoding: "utf8",
-  });
-
-  // Trim any text surrounding the stdout from the executed command. We are only
-  // interested in the printed out JSON.
-  let foundStart = false;
-  let foundEnd = false;
-  const firebaseEnvironmentalVariablesJson = stdout
-    .split("\n")
-    .filter(l => {
-      if (!foundStart && l !== "{") return false;
-      if (!foundStart && l === "{") {
-        foundStart = true;
-        return true;
-      }
-      if (!foundEnd && l === "}") {
-        foundEnd = true;
-        return true;
-      }
-      if (foundEnd) return false;
-      return true;
-    })
-    .join("\n");
-
-  // "functions.config(...)" is interested in a file called
-  // ".runtimeconfig.json" which includes the environmental variables.
-  fs.writeFileSync(
-    path.resolve(__dirname, "../.runtimeconfig.json"),
-    firebaseEnvironmentalVariablesJson,
-    "utf8",
-  );
-
-  /* tslint:disable-next-line:no-console */
-  console.log("...done.");
 }
