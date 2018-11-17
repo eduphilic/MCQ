40b127f92962bf84707928454d78e923fb697202
strothj <jason@jasons.io>
2018-11-17 03:48:01 -0500

    chore: remove storybook

--- storybook/stories/AdminImageUploadPanel.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { storiesOf } from "@storybook/react";
-import React from "react";
-import { AdminImageUploadPanel } from "../../src/features/admin";
-
-const stories = storiesOf("AdminImageUploadPanel", module);
-
-stories.addDecorator(story => <div style={{ margin: 16 }}>{story()}</div>);
-
-stories.add("in card header", () => <AdminImageUploadPanel />);
