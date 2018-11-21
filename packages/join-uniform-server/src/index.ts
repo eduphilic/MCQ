c503f74984deba680329d28c7f69cee2c9a91718
strothj <jason@jasons.io>
2018-11-21 17:34:28 -0500

    chore: add next ssr library

--- packages/join-uniform-server/src/index.ts
+++ packages/join-uniform-server/src/index.ts
@@ -1,6 +1,29 @@
 import Koa from "koa";
+import Router from "koa-router";
 import * as functions from "firebase-functions";
+import next from "next";
+import proxy from "koa-better-http-proxy";
 
-const app = new Koa();
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev, conf: { distDir: "next" } });
+const handle = nextApp.getRequestHandler();
 
-export const main = functions.https.onRequest(app.callback());
+const koaApp = new Koa();
+const router = new Router();
+
+if (!dev) {
+  router.all("*", async ctx => {
+    await nextApp.prepare();
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+}
+
+if (dev) {
+  router.all("*", proxy("localhost", { port: 3000 }));
+}
+
+koaApp.use(router.routes());
+koaApp.use(router.allowedMethods());
+
+export const main = functions.https.onRequest(koaApp.callback());
---
+++ tools/generateFirebaseFunctionsPackageJson.ts
@@ -0,0 +1,53 @@
+// Generate a "package.json" file for the Firebase Functions directory
+// "/dist/functions". This is needed because deployed functions need to have all
+// dependencies listed.
+
+import fs from "fs";
+import path from "path";
+
+const packageDirectories = fs
+  .readdirSync(path.resolve(__dirname, "../packages"))
+  .map(directoryName => path.resolve(__dirname, "../packages", directoryName));
+
+const dependencies = packageDirectories.reduce(
+  (accumulator, directoryPath) => {
+    const packageJsonPath = path.resolve(directoryPath, "package.json");
+    if (!fs.existsSync(packageJsonPath)) return accumulator;
+
+    const packageJson: {
+      dependencies?: Record<string, string>;
+    } = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+
+    if (!packageJson.dependencies) return accumulator;
+
+    Object.entries(packageJson.dependencies).forEach(([key, value]) => {
+      accumulator[key] = value;
+    });
+
+    return accumulator;
+  },
+  {} as Record<string, string>,
+);
+
+if (!fs.existsSync(path.resolve(__dirname, "../dist"))) {
+  fs.mkdirSync(path.resolve(__dirname, "../dist"));
+}
+
+if (!fs.existsSync(path.resolve(__dirname, "../dist/functions"))) {
+  fs.mkdirSync(path.resolve(__dirname, "../dist/functions"));
+}
+
+fs.writeFileSync(
+  path.resolve(__dirname, "../dist/functions/package.json"),
+  `${JSON.stringify(
+    {
+      private: true,
+      name: "@join-uniform/firebase-functions",
+      version: "1.0.0",
+      dependencies,
+    },
+    null,
+    2,
+  )}\n`,
+  "utf8",
+);
