a2ea07eb4b07c5faab3c118af44cf69b62df114b
strothj <jason@jasons.io>
2018-12-02 19:05:38 -0500

    chore: add static assets

--- packages/join-uniform-app/pages/_document.tsx
+++ packages/join-uniform-app/pages/_document.tsx
@@ -12,6 +12,8 @@ import { initializeApollo } from "../lib/rendering";
 
 type MyDocumentProps = Omit<GetHtmlConfigQuery, "__typename">;
 
+const publicPath = "/static";
+
 export default class MyDocument extends Document<MyDocumentProps> {
   static async getInitialProps(ctx: NextDocumentContext) {
     const initialDocumentProps = await Document.getInitialProps(ctx);
@@ -48,7 +50,14 @@ export default class MyDocument extends Document<MyDocumentProps> {
 
   render() {
     const {
-      htmlConfig: { googleAnalyticsId },
+      htmlConfig: {
+        googleAnalyticsId,
+        metaKeywords,
+        metaDescription,
+        metaAuthor,
+        metaAbstract,
+        metaCopyright,
+      },
     } = this.props;
 
     return (
@@ -82,6 +91,38 @@ export default class MyDocument extends Document<MyDocumentProps> {
               />
             </>
           )}
+
+          {/* Manifest and Favicon */}
+          {/* prettier-ignore */}
+          <>
+            <link rel="apple-touch-icon" sizes="57x57" href={`${publicPath}/favicons/apple-icon-57x57.png`} />
+            <link rel="apple-touch-icon" sizes="60x60" href={`${publicPath}/favicons/apple-icon-60x60.png`} />
+            <link rel="apple-touch-icon" sizes="72x72" href={`${publicPath}/favicons/apple-icon-72x72.png`} />
+            <link rel="apple-touch-icon" sizes="76x76" href={`${publicPath}/favicons/apple-icon-76x76.png`} />
+            <link rel="apple-touch-icon" sizes="114x114" href={`${publicPath}/favicons/apple-icon-114x114.png`} />
+            <link rel="apple-touch-icon" sizes="120x120" href={`${publicPath}/favicons/apple-icon-120x120.png`} />
+            <link rel="apple-touch-icon" sizes="144x144" href={`${publicPath}/favicons/apple-icon-144x144.png`} />
+            <link rel="apple-touch-icon" sizes="152x152" href={`${publicPath}/favicons/apple-icon-152x152.png`} />
+            <link rel="apple-touch-icon" sizes="180x180" href={`${publicPath}/favicons/apple-icon-180x180.png`} />
+            <link rel="icon" type="image/png" sizes="192x192" href={`${publicPath}/favicons/android-icon-192x192.png`} />
+            <link rel="icon" type="image/png" sizes="32x32" href={`${publicPath}/favicons/favicon-32x32.png`} />
+            <link rel="icon" type="image/png" sizes="96x96" href={`${publicPath}/favicons/favicon-96x96.png`} />
+            <link rel="icon" type="image/png" sizes="16x16" href={`${publicPath}/favicons/favicon-16x16.png`} />
+            <link rel="shortcut icon" href={`${publicPath}/favicon.ico`} />
+            <meta name="msapplication-TileColor" content="#ffffff" />
+            <meta name="msapplication-TileImage" content={`${publicPath}/favicons/ms-icon-144x144.png`} />
+            <meta name="theme-color" content="#ffffff" />
+            <link rel="manifest" href={`${publicPath}/manifest.json`} />
+          </>
+
+          {/* Keywords and Meta Tags */}
+          {metaKeywords && <meta name="keywords" content={metaKeywords} />}
+          {metaDescription && (
+            <meta name="description" content={metaDescription} />
+          )}
+          {metaAuthor && <meta name="author" content={metaAuthor} />}
+          {metaAbstract && <meta name="abstract" content={metaAbstract} />}
+          {metaCopyright && <meta name="copyright" content={metaCopyright} />}
         </Head>
 
         <body>
