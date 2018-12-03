3e4303632e0a80c68a37d14861eb7d32309477fc
strothj <jason@jasons.io>
2018-12-02 19:00:54 -0500

    chore: move html page meta data query to server side render

--- packages/join-uniform-app/layouts/Html.tsx
+++ packages/join-uniform-app/layouts/Html.tsx
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import React from "react";
-import { GetHtmlConfigHOC } from "../graphql";
+import React, { ReactNode } from "react";
 
 type Props = {
+  children?: ReactNode;
   title: string;
 };
 
@@ -10,63 +10,16 @@ type Props = {
  * Adds meta tags and page title to pages. It is used as a base by other
  * layouts.
  */
-export const Html = GetHtmlConfigHOC<Props, {}>({})(props => {
-  const { children, data } = props;
-  const { loading, error, htmlConfig } = data!;
-  if (loading || error || !htmlConfig) return null;
-
-  const { googleAnalyticsId } = htmlConfig;
+export function Html(props: Props) {
+  const { children, title } = props;
 
   return (
     <>
       <Head>
-        <title key="title">{props.title}</title>
-
-        <meta key="charSet" charSet="utf-8" />
-        <meta key="httpEquiv" httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          key="viewport"
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        />
-
-        {/* Material UI Font */}
-        <link
-          key="font"
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700"
-        />
-
-        {/* Google Analytics */}
-        {process.env.NODE_ENV === "production" && googleAnalyticsId && (
-          <>
-            <script
-              key="googleTagManager"
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
-            />
-            <script
-              key="googleAnalytics"
-              dangerouslySetInnerHTML={{
-                __html: generateGoogleAnalyticsScriptContents(
-                  googleAnalyticsId,
-                ),
-              }}
-            />
-          </>
-        )}
+        <title key="title">{title}</title>
       </Head>
 
       {children}
     </>
   );
-});
-
-function generateGoogleAnalyticsScriptContents(googleAnalyticsId: string) {
-  return `window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', '${googleAnalyticsId}');`
-    .split("\n")
-    .join("");
 }
--- packages/join-uniform-app/pages/_document.tsx
+++ packages/join-uniform-app/pages/_document.tsx
@@ -1,9 +1,27 @@
-import NextDocument, { NextDocumentContext } from "next/document";
+import Document, {
+  DefaultDocumentIProps,
+  Head,
+  Main,
+  NextDocumentContext,
+  NextScript,
+} from "next/document";
 import React from "react";
 import { ServerStyleSheet } from "styled-components";
+import { GetHtmlConfigDocument, GetHtmlConfigQuery } from "../graphql";
+import { initializeApollo } from "../lib/rendering";
 
-export default class Document extends NextDocument {
+type MyDocumentProps = Omit<GetHtmlConfigQuery, "__typename">;
+
+export default class MyDocument extends Document<MyDocumentProps> {
   static async getInitialProps(ctx: NextDocumentContext) {
+    const initialDocumentProps = await Document.getInitialProps(ctx);
+
+    const apolloClient = initializeApollo();
+    const htmlConfigQueryResult = await apolloClient.query<GetHtmlConfigQuery>({
+      query: GetHtmlConfigDocument,
+    });
+    const htmlConfig = htmlConfigQueryResult.data.htmlConfig;
+
     const sheet = new ServerStyleSheet();
 
     const originalRenderPage = ctx.renderPage;
@@ -12,11 +30,74 @@ export default class Document extends NextDocument {
         sheet.collectStyles(<App {...props} />),
       );
 
-    const initialProps = await NextDocument.getInitialProps(ctx);
+    const styles = (
+      <>
+        {initialDocumentProps.styles}
+        {sheet.getStyleElement()}
+      </>
+    );
 
-    return {
-      ...initialProps,
-      styles: [...(initialProps.styles as any), ...sheet.getStyleElement()],
+    const initialProps: DefaultDocumentIProps & MyDocumentProps = {
+      ...initialDocumentProps,
+      styles,
+      htmlConfig,
     };
+
+    return initialProps;
   }
+
+  render() {
+    const {
+      htmlConfig: { googleAnalyticsId },
+    } = this.props;
+
+    return (
+      <html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+          {/* Material UI Font */}
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700"
+          />
+
+          {/* Google Analytics */}
+          {process.env.NODE_ENV === "production" && googleAnalyticsId && (
+            <>
+              <script
+                key="googleTagManager"
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+              />
+              <script
+                key="googleAnalytics"
+                dangerouslySetInnerHTML={{
+                  __html: generateGoogleAnalyticsScriptContents(
+                    googleAnalyticsId,
+                  ),
+                }}
+              />
+            </>
+          )}
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+
+function generateGoogleAnalyticsScriptContents(googleAnalyticsId: string) {
+  return `window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', '${googleAnalyticsId}');`
+    .split("\n")
+    .join("");
 }
