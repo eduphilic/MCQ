21af68fe49844dff7e5c64f62c1846a70dc7397c
strothj <jason@jasons.io>
2018-12-12 01:11:27 -0500

    chore: refactor

--- packages/join-uniform-app/pages/admin/entry-manager.tsx
+++ packages/join-uniform-app/pages/admin/entry-manager.tsx
@@ -9,29 +9,29 @@ import { withQueryLoadingSpinner } from "../../lib/utils";
 export default function AdminIndexPage() {
   return (
     <AdminLayoutDashboardContainer title="Entry Manager">
-      {withQueryLoadingSpinner(GetEntriesComponent, entriesResult =>
-        entriesResult.data.entries.map(entry => (
-          <Fragment key={entry.id}>
-            <div>{entry.name}</div>
-            {entry.categories.map(categoryId =>
-              withQueryLoadingSpinner(
-                GetCategoryComponent,
-                { key: categoryId, variables: { id: categoryId } },
-                categoryResult => (
-                  <div>
-                    {JSON.stringify(
-                      categoryResult.data.category || "",
-                      null,
-                      2,
-                    )}
-                  </div>
-                ),
-              ),
-            )}
-            <br />
-          </Fragment>
-        )),
-      )}
+      {renderEntries()}
     </AdminLayoutDashboardContainer>
   );
+
+  function renderEntries() {
+    return withQueryLoadingSpinner(GetEntriesComponent, entriesResult =>
+      entriesResult.data.entries.map(entry => (
+        <Fragment key={entry.id}>
+          <div>{entry.name}</div>
+          {entry.categories.map(categoryId => renderCategory(categoryId))}
+          <br />
+        </Fragment>
+      )),
+    );
+  }
+
+  function renderCategory(categoryId: string) {
+    return withQueryLoadingSpinner(
+      GetCategoryComponent,
+      { key: categoryId, variables: { id: categoryId } },
+      categoryResult => (
+        <div>{JSON.stringify(categoryResult.data.category || "", null, 2)}</div>
+      ),
+    );
+  }
 }
