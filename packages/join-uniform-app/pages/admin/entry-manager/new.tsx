8dd6998c404929ed4bc18691b5a027b671400ee4
strothj <jason@jasons.io>
2018-12-14 01:27:44 -0500

    feat: start existing entry section

--- packages/join-uniform-app/pages/admin/entry-manager/new.tsx
+++ packages/join-uniform-app/pages/admin/entry-manager/new.tsx
@@ -2,33 +2,81 @@ import {
   Card,
   CardContent,
   CardHeader,
+  FormControl,
+  FormControlLabel,
   Grid,
   PendingChangesButton,
+  Radio,
+  RadioGroup,
 } from "@join-uniform/components";
+import { GetEntriesComponent } from "@join-uniform/graphql";
+import { Divider } from "@material-ui/core";
+import { Formik } from "formik";
 import React from "react";
 import { AdminLayoutDashboardContainer } from "../../../containers";
+import { withQueryLoadingSpinner } from "../../../lib/utils";
+
+type FormValues = {
+  entrySource: "existing" | "new";
+};
 
 export default function EntryManagerNew() {
-  return (
-    <AdminLayoutDashboardContainer
-      title="New Entry"
-      appBarButtons={[
-        <PendingChangesButton
-          hasDiscardableChanges
-          hasPublishableChanges
-          onDiscardButtonClick={() => {}} // tslint:disable-line:no-empty
-          onPublishButtonClick={() => {}} // tslint:disable-line:no-empty
-        />,
-      ]}
-    >
-      <Grid container spacing={16} contentCenter>
-        <Grid item xs>
-          <Card>
-            <CardHeader title="Entry Selection" />
-            <CardContent>Existing entry / create new entry</CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-    </AdminLayoutDashboardContainer>
+  return withQueryLoadingSpinner(
+    GetEntriesComponent,
+    ({ data: { entries } }) => (
+      <Formik<FormValues>
+        initialValues={{
+          entrySource: entries.length > 0 ? "existing" : "new",
+        }}
+        onSubmit={() => {}}
+      >
+        {form => (
+          <AdminLayoutDashboardContainer
+            title="New Entry"
+            appBarButtons={[
+              <PendingChangesButton
+                hasDiscardableChanges
+                hasPublishableChanges
+                onDiscardButtonClick={() => {}} // tslint:disable-line:no-empty
+                onPublishButtonClick={() => {}} // tslint:disable-line:no-empty
+              />,
+            ]}
+          >
+            <Grid container spacing={16} contentCenter>
+              <Grid item xs>
+                <Card>
+                  <CardHeader title="Entry Selection" />
+                  <CardContent>
+                    <FormControl>
+                      <RadioGroup
+                        name="entrySource"
+                        value={form.values.entrySource}
+                        onChange={form.handleChange}
+                        onBlur={form.handleBlur}
+                      >
+                        <FormControlLabel
+                          value="existing"
+                          control={<Radio />}
+                          label="Existing entry"
+                        />
+                        <FormControlLabel
+                          value="new"
+                          control={<Radio />}
+                          label="Create new entry"
+                        />
+                      </RadioGroup>
+                    </FormControl>
+                  </CardContent>
+                  <Divider />
+                  {form.values.entrySource === "existing" && (
+                    <CardContent>Placeholder</CardContent>
+                  )}
+                </Card>
+              </Grid>
+            </Grid>
+          </AdminLayoutDashboardContainer>
+        )}
+      </Formik>
+    ),
   );
 }
--- packages/join-uniform-components/src/muiComponents.ts
+++ packages/join-uniform-components/src/muiComponents.ts
@@ -4,6 +4,24 @@ import CardContent, {
 export type CardContentProps = MuiCardContentProps;
 export { CardContent };
 
+import FormControl, {
+  FormControlProps as MuiFormControlProps,
+} from "@material-ui/core/FormControl";
+export type FormControlProps = MuiFormControlProps;
+export { FormControl };
+
+import FormControlLabel, {
+  FormControlLabelProps as MuiFormControlLabelProps,
+} from "@material-ui/core/FormControlLabel";
+export type FormControlLabelProps = MuiFormControlLabelProps;
+export { FormControlLabel };
+
+import FormLabel, {
+  FormLabelProps as MuiFormLabelProps,
+} from "@material-ui/core/FormLabel";
+export type FormLabelProps = MuiFormLabelProps;
+export { FormLabel };
+
 import Hidden, {
   HiddenProps as MuiHiddenProps,
 } from "@material-ui/core/Hidden";
@@ -19,6 +37,16 @@ import IconButton, {
 } from "@material-ui/core/IconButton";
 export type IconButtonProps = MuiIconButtonProps;
 export { IconButton };
+
+import Radio, { RadioProps as MuiRadioProps } from "@material-ui/core/Radio";
+export type RadioProps = MuiRadioProps;
+export { Radio };
+
+import RadioGroup, {
+  RadioGroupProps as MuiRadioGroupProps,
+} from "@material-ui/core/RadioGroup";
+export type RadioGroupProps = MuiRadioGroupProps;
+export { RadioGroup };
 
 import Typography, {
   TypographyProps as MuiTypographyProps,
