9ccc0c6d0fc1f6a88078d7e66636d7226336b339
strothj <jason@jasons.io>
2018-12-21 00:40:24 -0500

    feat: add list sort and drag handler

--- packages/join-uniform-components/src/DraggableList/DraggableList.tsx
+++ packages/join-uniform-components/src/DraggableList/DraggableList.tsx
@@ -1,13 +1,22 @@
-import React, { ReactElement, ReactNode } from "react";
+import { DragHandleIcon, RemoveIcon } from "@join-uniform/icons";
+import { styled } from "@join-uniform/theme";
+import React, { ReactElement } from "react";
 import {
   arrayMove,
   SortableContainer,
+  SortableContainerProps,
   SortableElement,
   SortableHandle,
 } from "react-sortable-hoc";
 import { Grid } from "../Grid";
+import { IconButton } from "../muiComponents";
 
-export type DraggableListProps = {
+export { arrayMove };
+
+export type DraggableListProps = Omit<
+  SortableContainerProps,
+  "useDragHandle"
+> & {
   itemElements: ReactElement<any>[];
 };
 
@@ -15,7 +24,7 @@ type DraggableListItemProps = {
   children: ReactElement<any>;
 };
 
-export const DraggableList = SortableContainer<DraggableListProps>(props => {
+const DraggableListBase = SortableContainer<DraggableListProps>(props => {
   const { itemElements } = props;
 
   const draggableItemElements = itemElements.map((itemElement, index) => {
@@ -30,15 +39,44 @@ export const DraggableList = SortableContainer<DraggableListProps>(props => {
   return <div>{draggableItemElements}</div>;
 });
 
+export function DraggableList(props: DraggableListProps) {
+  return <DraggableListBase {...props} useDragHandle />;
+}
+
 const DraggableListItem = SortableElement<DraggableListItemProps>(props => {
   const { children } = props;
 
   return (
     <Grid container spacing={16}>
-      <Grid item>list_controls</Grid>
+      <Grid item>
+        <Grid container direction="column">
+          <Grid item>
+            <DragHandle />
+          </Grid>
+          <Grid item>
+            <IconButton>
+              <RemoveIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Grid>
       <Grid item xs>
         {children}
       </Grid>
     </Grid>
   );
 });
+
+const DragHandleIconButton = styled((props: { className?: string }) => (
+  <IconButton className={props.className}>
+    <DragHandleIcon />
+  </IconButton>
+))`
+  cursor: grab;
+`;
+
+const DragHandle = SortableHandle(() => (
+  <div>
+    <DragHandleIconButton />
+  </div>
+));
--- packages/join-uniform-icons/src/index.ts
+++ packages/join-uniform-icons/src/index.ts
@@ -6,7 +6,9 @@ export { default as CollectionsIcon } from "@material-ui/icons/Collections";
 export { default as DashboardIcon } from "@material-ui/icons/Dashboard";
 export { default as DeleteIcon } from "@material-ui/icons/Delete";
 export { default as DoneIcon } from "@material-ui/icons/Done";
+export { default as DragHandleIcon } from "@material-ui/icons/DragHandle";
 export { default as EditIcon } from "@material-ui/icons/Edit";
 export { default as MenuIcon } from "@material-ui/icons/Menu";
 export { default as PowerSettingsNewIcon } from "@material-ui/icons/PowerSettingsNew"; // prettier-ignore
+export { default as RemoveIcon } from "@material-ui/icons/Remove";
 export { default as TextureIcon } from "@material-ui/icons/Texture";
