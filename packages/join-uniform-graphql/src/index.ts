34169886e051e15fc74fa9c5299c93a105a70cc5
strothj <jason@jasons.io>
2018-12-22 09:37:09 -0500

    fix: compiler error due to export collision

--- packages/join-uniform-graphql/src/index.ts
+++ packages/join-uniform-graphql/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./models";
 export * from "./validations";
+// @ts-ignore
 export * from "./generated.client";
--- packages/join-uniform-graphql/src/server.ts
+++ packages/join-uniform-graphql/src/server.ts
@@ -2,4 +2,5 @@ export * from "./models";
 export * from "./typeDefs";
 export * from "./validations";
 export * from "./firebaseRemoteConfigSchema";
+// @ts-ignore
 export * from "./generated.server";
