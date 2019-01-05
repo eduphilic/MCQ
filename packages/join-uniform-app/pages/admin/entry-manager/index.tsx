2adddb1ed7d975eedcf4a4b37443db682feb0914
strothj <jason@jasons.io>
2019-01-05 08:10:28 -0500

    feat: update apollo store with category activation toggles

--- packages/join-uniform-app/pages/admin/entry-manager/index.tsx
+++ packages/join-uniform-app/pages/admin/entry-manager/index.tsx
@@ -15,6 +15,9 @@ import {
   EntryManagerGetEntriesEntries,
   EntryManagerGetEntriesQuery,
   EntryManagerGetEntriesVariables,
+  EntryManagerSetCategoryActivationStatusesComponent,
+  EntryManagerSetCategoryActivationStatusesMutation,
+  EntryManagerSetCategoryActivationStatusesVariables,
 } from "@join-uniform/graphql";
 import { AddIcon } from "@join-uniform/icons";
 import { FormikHelpers, useFormik } from "formik";
@@ -43,6 +46,10 @@ type RenderProps = {
     EntryManagerDeleteCategoriesMutation,
     EntryManagerDeleteCategoriesVariables
   >;
+  setCategoryActivationStatuses: MutationFn<
+    EntryManagerSetCategoryActivationStatusesMutation,
+    EntryManagerSetCategoryActivationStatusesVariables
+  >;
 };
 
 type Props = Omit<RenderProps, "getEntriesResult"> & {
@@ -53,6 +60,9 @@ const Composed = adopt<RenderProps, {}>({
   getEntriesResult: <EntryManagerGetEntriesComponent />,
   deleteEntries: <EntryManagerDeleteEntriesComponent />,
   deleteCategories: <EntryManagerDeleteCategoriesComponent />,
+  setCategoryActivationStatuses: (
+    <EntryManagerSetCategoryActivationStatusesComponent />
+  ),
 });
 
 export default function PageContainer() {
@@ -73,7 +83,12 @@ export default function PageContainer() {
 }
 
 function Page(props: Props) {
-  const { entries, deleteEntries, deleteCategories } = props;
+  const {
+    entries,
+    deleteEntries,
+    deleteCategories,
+    setCategoryActivationStatuses,
+  } = props;
   const form = useFormik<FormValues>({
     enableReinitialize: true,
     initialValues: { entries },
@@ -163,6 +178,37 @@ function Page(props: Props) {
     values: FormValues,
     helpers: FormikHelpers<FormValues>,
   ) {
+    // Handle Category activation toggling.
+    const initialCategories = entries.flatMap(e => e.categories);
+    const activationToggledCategories = values.entries
+      .flatMap(e => e.categories)
+      .filter(
+        c =>
+          initialCategories.find(cc => cc.id === c.id)!.activated !==
+          c.activated,
+      );
+    if (activationToggledCategories.length > 0) {
+      await setCategoryActivationStatuses({
+        variables: {
+          categoryIds: activationToggledCategories.map(c => c.id),
+          activationStatuses: activationToggledCategories.map(c => c.activated),
+        },
+        update: proxy => {
+          updateStoreEntries(proxy, queryResult => {
+            queryResult.entries
+              .flatMap(e => e.categories)
+              .forEach(category => {
+                const update = activationToggledCategories.find(
+                  cc => cc.id === category.id,
+                );
+                if (!update) return;
+                category.activated = update.activated;
+              });
+          });
+        },
+      });
+    }
+
     // Handle Category deletions.
     const initialCategoryIds = entries.flatMap(e =>
       e.categories.map(c => c.id),
@@ -212,185 +258,3 @@ function Page(props: Props) {
     helpers.setSubmitting(false);
   }
 }
-
-// import {
-//   Button,
-//   DashboardCard,
-//   DashboardCardItem,
-//   Grid,
-//   Typography,
-// } from "@join-uniform/components";
-// import {
-//   DeleteCategoriesComponent,
-//   DeleteCategoriesMutation,
-//   DeleteCategoriesVariables,
-//   DeleteEntryComponent,
-//   GetEntriesComponent,
-//   GetEntriesEntries,
-//   GetEntryCategoriesComponent,
-//   GetEntryCategoriesEntryCategories,
-//   SetCategoryActivationStatusComponent,
-// } from "@join-uniform/graphql";
-// import { AddIcon } from "@join-uniform/icons";
-// import { css, styled } from "@join-uniform/theme";
-// import Link from "next/link";
-// import Router from "next/router";
-// import React from "react";
-// import { MutationFn } from "react-apollo";
-// import { AdminLayoutDashboardContainer } from "~/containers";
-// import {
-//   createResponsiveImageUrl,
-//   urlHashCodeEncode,
-//   withQueryLoadingSpinner,
-// } from "~/lib/utils";
-
-// export default function AdminEntryManagerIndexPage() {
-//   return (
-//     <AdminLayoutDashboardContainer
-//       title="Entry Manager"
-//       appBarButtons={[
-//         <Link href="/admin/entry-manager/new">
-//           <Button color="orange">
-//             {/* prettier-ignore */}
-//             <AddIcon css={css`margin-right: 8px;`} />
-//             Entry
-//           </Button>
-//         </Link>,
-//       ]}
-//     >
-//       <Grid container contentCenter spacing={16}>
-//         <DeleteCategoriesComponent>
-//           {deleteCategories =>
-//             withQueryLoadingSpinner(GetEntriesComponent, entriesResult =>
-//               entriesResult.data.entries.map(entry =>
-//                 withQueryLoadingSpinner(
-//                   GetEntryCategoriesComponent,
-//                   { key: entry.id, variables: { entryId: entry.id } },
-//                   categoriesResult =>
-//                     renderEntryCard(
-//                       entry,
-//                       categoriesResult.data.entryCategories,
-//                       entriesResult.refetch,
-//                       categoriesResult.refetch,
-//                       deleteCategories,
-//                     ),
-//                 ),
-//               ),
-//             )
-//           }
-//         </DeleteCategoriesComponent>
-//       </Grid>
-//     </AdminLayoutDashboardContainer>
-//   );
-
-//   function renderEntryCard(
-//     entry: GetEntriesEntries,
-//     categories: GetEntryCategoriesEntryCategories[],
-//     entriesRefetch: () => any,
-//     categoriesRefetch: () => any,
-//     deleteCategories: MutationFn<
-//       DeleteCategoriesMutation,
-//       DeleteCategoriesVariables
-//     >,
-//   ) {
-//     return (
-//       <SetCategoryActivationStatusComponent>
-//         {setCategoryActivationStatus => (
-//           <DeleteEntryComponent>
-//             {deleteEntry => (
-//               <Grid key={entry.id} item xs={12}>
-//                 <DashboardCard
-//                   title={`${entry.name} Entry`}
-//                   titleSiblingNode={
-//                     <Typography variant="subtitle1">
-//                       #{urlHashCodeEncode(entry.name)}
-//                     </Typography>
-//                   }
-//                   iconNode={<EntryLogoImageIcon logoUrl={entry.logoUrl} />}
-//                   columnLabels={[
-//                     "Category",
-//                     "Cost Per Paper (Rs)",
-//                     "Activated",
-//                   ]}
-//                   columnTypes={["dual-line", "single-line", "switch"]}
-//                   bottomActionsNode={
-//                     <>
-//                       <Button
-//                         onClick={() => {
-
-//                         }}
-//                       >
-//                         Edit Entry
-//                       </Button>
-//                       <Button
-//                         disabled={categories.length > 0}
-//                         onClick={async () => {
-//                           if (
-//                             !confirm(
-//                               "Are you sure you want to delete this Entry?",
-//                             )
-//                           ) {
-//                             return;
-//                           }
-
-//                           await deleteEntry({
-//                             variables: {
-//                               entryId: entry.id,
-//                             },
-//                           });
-//                           await entriesRefetch();
-//                         }}
-//                       >
-//                         Delete Entry
-//                       </Button>
-//                     </>
-//                   }
-//                   onItemEditClick={categoryId => {
-//                     // tslint:disable-next-line:no-floating-promises
-//                     Router.push(
-//                       `/admin/entry-manager/edit-category?categoryId=${categoryId}`,
-//                     );
-//                   }}
-//                   onRequestDeleteClick={async categoryIds => {
-//                     if (!confirm("Remove the selected categories?")) return;
-//                     await deleteCategories({
-//                       variables: { entryId: entry.id, categoryIds },
-//                     });
-//                     await categoriesRefetch();
-//                   }}
-//                   items={categories.map(
-//                     (category): DashboardCardItem => ({
-//                       key: category.id,
-//                       columns: [
-//                         {
-//                           primaryText: category.name,
-//                           secondaryText: category.education,
-//                         },
-//                         {
-//                           primaryText: category.pricePerPaperRs.toString(),
-//                         },
-//                         {
-//                           switchChecked: category.activated,
-//                           switchTooltipTitle: "Toggle Activation",
-//                           switchOnChange: async checked => {
-//                             await setCategoryActivationStatus({
-//                               variables: {
-//                                 categoryId: category.id,
-//                                 activated: checked,
-//                               },
-//                             });
-//                             await categoriesRefetch();
-//                           },
-//                         },
-//                       ],
-//                     }),
-//                   )}
-//                 />
-//               </Grid>
-//             )}
-//           </DeleteEntryComponent>
-//         )}
-//       </SetCategoryActivationStatusComponent>
-//     );
-//   }
-// }
