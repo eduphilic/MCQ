fe6a66fc90ecc50297ab0d9b459eba74b6b85fb6
strothj <jason@jasons.io>
2019-01-12 02:59:51 -0500

    feat: stitch schema for tooling support

---
+++ tools/stitchSchema.ts
@@ -0,0 +1,62 @@
+// tslint:disable:no-implicit-dependencies
+import fs from "fs";
+import { printSchema } from "graphql";
+import { makeExecutableSchema } from "graphql-tools";
+import path from "path";
+
+const fsPromises = fs.promises;
+const schemaDirectoryPath = path.join(
+  __dirname,
+  "../packages/join-uniform-server/src/resolvers",
+);
+const stitchedSchemaOutputPath = path.resolve(
+  __dirname,
+  "../schema.generated.graphql",
+);
+
+async function main() {
+  const schemaFilePaths = await getSchemaFilePaths(schemaDirectoryPath);
+  const schemaFiles = await Promise.all(
+    schemaFilePaths.map(schemaFilePath =>
+      fsPromises.readFile(schemaFilePath, "utf8"),
+    ),
+  );
+
+  const typeDefs = schemaFiles.join("\n");
+  const schema = makeExecutableSchema({ typeDefs });
+
+  const stitchedSchema = `# This is an automatically generated file.
+# DO NOT MODIFY
+# Use the command: "yarn graphql" to regenerate.
+
+${printSchema(schema, { commentDescriptions: true })}`;
+
+  await fsPromises.writeFile(stitchedSchemaOutputPath, stitchedSchema, "utf8");
+}
+
+async function getSchemaFilePaths(directoryPath: string) {
+  const directoryNodeFilenames = await fsPromises.readdir(directoryPath);
+  const schemaFilePaths: string[] = [];
+
+  await Promise.all(
+    directoryNodeFilenames.map(async nodeFilename => {
+      const nodePath = path.join(directoryPath, nodeFilename);
+
+      if (/\.graphql$/.test(nodePath)) {
+        schemaFilePaths.push(nodePath);
+        return;
+      }
+
+      const isDirectory = (await fsPromises.stat(nodePath)).isDirectory();
+      if (isDirectory) {
+        const additionalSchemaFilePaths = await getSchemaFilePaths(nodePath);
+        schemaFilePaths.push(...additionalSchemaFilePaths);
+      }
+    }),
+  );
+
+  return schemaFilePaths;
+}
+
+// tslint:disable-next-line:no-floating-promises
+main();
