b5a8fe9178155a2d0607d80e8ecf9c2caf5db4d0
strothj <jason@jasons.io>
2019-01-27 02:51:24 -0500

    chore: keep next.js from clearing console output

--- next.config.js
+++ next.config.js
@@ -3,4 +3,27 @@ const withTypescript = require("@zeit/next-typescript");
 
 module.exports = withTypescript({
   distDir: "dist/functions/.next",
+
+  // Prevent screen clear after builds.
+  /* eslint-disable no-param-reassign */
+  webpack(config, { dev, isServer }) {
+    if (dev && !isServer) {
+      const friendlyErrorsPluginInstance = config.plugins.find(
+        plugin => plugin.constructor.name === "FriendlyErrorsWebpackPlugin",
+      );
+      if (friendlyErrorsPluginInstance) {
+        const FriendlyErrorsWebpackPlugin =
+          friendlyErrorsPluginInstance.constructor;
+        config.plugins = config.plugins.filter(
+          plugin => plugin !== friendlyErrorsPluginInstance,
+        );
+        config.plugins.push(
+          new FriendlyErrorsWebpackPlugin({ clearConsole: false }),
+        );
+      }
+    }
+
+    return config;
+  },
+  /* eslint-enable no-param-reassign */
 });
