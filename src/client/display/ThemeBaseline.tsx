62023348b525adcd2eaabb2e6981fd5ee10cd1b9
strothj <jason@jasons.io>
2019-03-06 00:09:31 -0500

    feat: add theme and viewport meta tags

--- src/client/display/ThemeBaseline.tsx
+++ src/client/display/ThemeBaseline.tsx
@@ -1,4 +1,5 @@
 import { makeStyles, ThemeProvider } from "@material-ui/styles";
+import Head from "next/head";
 import React, { ReactNode } from "react";
 import "typeface-montserrat";
 import { theme } from "./theme";
@@ -24,5 +25,17 @@ const useStyles = makeStyles({
 export function ThemeBaseline({ children }: Props) {
   useStyles();
 
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+        />
+        <meta name="theme-color" content={theme.palette.primary.main} />
+      </Head>
+
+      {children}
+    </ThemeProvider>
+  );
 }
