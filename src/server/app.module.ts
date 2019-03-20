02c1fa3b48ab4f0d905d523df72939b24f81044d
strothj <jason@jasons.io>
2019-03-20 08:28:36 -0400

    refactor: cookie handling to ssr (in-progress)

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,21 +1,23 @@
 import {
-  MiddlewareConsumer,
+  // MiddlewareConsumer,
   Module,
-  NestModule,
-  RequestMethod,
+  // NestModule,
+  // RequestMethod,
 } from "@nestjs/common";
 import { ConfigModule } from "./config";
 import { LandingModule } from "./landing";
-import { SessionMiddleware, SessionModule } from "./session";
+// import { SessionMiddleware, SessionModule } from "./session";
 import { UserModule } from "./user";
 
 @Module({
-  imports: [ConfigModule, SessionModule, UserModule, LandingModule],
+  // imports: [ConfigModule, SessionModule, UserModule, LandingModule],
+  imports: [ConfigModule, UserModule, LandingModule],
 })
-export class ApplicationModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(SessionMiddleware)
-      .forRoutes({ path: "", method: RequestMethod.ALL });
-  }
+// export class ApplicationModule implements NestModule {
+export class ApplicationModule {
+  // configure(consumer: MiddlewareConsumer) {
+  //   consumer
+  //     .apply(SessionMiddleware)
+  //     .forRoutes({ path: "", method: RequestMethod.ALL });
+  // }
 }
