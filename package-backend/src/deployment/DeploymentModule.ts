f419d6e8d8ab1d88006050705008569dc9c6780c
strothj <jason@jasons.io>
2019-04-17 02:00:44 -0400

    feat: add firebase multer compatibility middleware

--- package-backend/src/deployment/DeploymentModule.ts
+++ package-backend/src/deployment/DeploymentModule.ts
@@ -1,9 +1,21 @@
-import { Module } from "@nestjs/common";
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from "@nestjs/common";
 import { ConfigModule } from "../config";
 import { DeploymentController } from "./DeploymentController";
+import { DeploymentMulterFirebaseCompatibilityMiddleware } from "./DeploymentMulterFirebaseCompatibilityMiddleware";
 
 @Module({
   imports: [ConfigModule],
   controllers: [DeploymentController],
 })
-export class DeploymentModule {}
+export class DeploymentModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(DeploymentMulterFirebaseCompatibilityMiddleware)
+      .forRoutes({ path: "/api/deploy", method: RequestMethod.POST });
+  }
+}
