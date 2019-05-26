d51d3f7fd37ae69b6979c8eff5bd49a5ecfa727b
strothj <jason@jasons.io>
2019-05-26 08:25:39 -0400

    chore: add jest and storybook snapshotting

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  coverageDirectory: "coverage",
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/test/fileMock.ts",
+  },
+  setupFiles: ["<rootDir>/src/test/registerContext.ts"],
+};
---
+++ src/types/babel-plugin-require-context-hook.d.ts
@@ -0,0 +1,5 @@
+declare module "babel-plugin-require-context-hook/register" {
+  function registerRequireContextHook(): void;
+
+  export = registerRequireContextHook;
+}
