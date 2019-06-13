6a41bc619ce59485d10cb281a0c5d0e0b26e879c
strothj <jason@jasons.io>
2019-06-13 05:32:33 -0400

    chore: add stricter omit type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -7,3 +7,14 @@ declare namespace NodeJS {
     browser: boolean;
   }
 }
+
+/**
+ * Strict version of the built in `Omit` helper. It enforces that the keys to be
+ * omitted actually exist on the target type.
+ *
+ * TypeScript 3.5 introduced the `Omit` type into the core language. It was
+ * decided that the helper would not be strict.
+ *
+ * @see https://github.com/Microsoft/TypeScript/pull/30552
+ */
+type OmitStrict<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
