bdef9297110f320ab78432a6cc48cbeea88a1c26
strothj <jason@jasons.io>
2019-06-13 05:32:20 -0400

    chore: add additional asset type definitions

--- src/types/assets.d.ts
+++ src/types/assets.d.ts
@@ -12,3 +12,19 @@ declare module "*.png" {
   const url: string;
   export = url;
 }
+
+declare module "*.jpg" {
+  const url: string;
+  export = url;
+}
+
+declare module "*.svg" {
+  import * as React from "react";
+
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+
+  const src: string;
+  export default src;
+}
