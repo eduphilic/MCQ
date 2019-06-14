d15858eb972403a0441995abb05263e9825160c7
strothj <jason@jasons.io>
2019-06-14 04:45:10 -0400

    chore: mount frontend prototype under index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,9 @@
 import React from "react";
 import { StickyFooter } from "../src/components";
+import App from "../src/prototype/App";
 
-export default () => <StickyFooter>Placeholder</StickyFooter>;
+export default () => (
+  <StickyFooter>
+    <App />
+  </StickyFooter>
+);
