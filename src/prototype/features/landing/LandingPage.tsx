d8e89c38cd11a903d9f5e5ad8178159722802110
strothj <jason@jasons.io>
2019-06-18 10:18:58 -0400

    fix: disable youtube videos component due to api key error

--- src/prototype/features/landing/LandingPage.tsx
+++ src/prototype/features/landing/LandingPage.tsx
@@ -4,14 +4,16 @@ import { PageFooter } from "../../componentsV0/PageFooter";
 import { Hero } from "./components/Hero";
 import { IndexCards } from "./components/IndexCards";
 import { LandingTemplate } from "./components/LandingTemplate";
-import { YouTubeVideos } from "./components/YouTubeVideos";
+// import { YouTubeVideos } from "./components/YouTubeVideos";
 
 export const LandingPage: SFC<{}> = () => {
   return (
     <LandingTemplate
       heroNode={<Hero />}
       testCardNode={<IndexCards />}
-      youTubeVideosNode={<YouTubeVideos />}
+      // TODO: Add mechanism to safely retrieve API key and video listings.
+      // youTubeVideosNode={<YouTubeVideos />}
+      youTubeVideosNode={<></>}
       footerNode={<PageFooter />}
     />
   );
