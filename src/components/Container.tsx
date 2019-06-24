377b5899ba5291680a5c448b326dbd15afeba6da
strothj <jason@jasons.io>
2019-06-24 05:18:22 -0400

    refactor: expose props

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,14 +1,14 @@
 import { Container as MuiContainer } from "@material-ui/core";
-import { ContainerProps } from "@material-ui/core/Container";
+import { ContainerProps as MuiContainerProps } from "@material-ui/core/Container";
 import { useContext } from "react";
 import { ThemeContext } from "styled-components";
 
-type Props = OmitStrict<ContainerProps, "maxWidth">;
+export type ContainerProps = OmitStrict<MuiContainerProps, "maxWidth">;
 
 /**
  * Centers the page contents and provides horizontal margins.
  */
-export function Container(props: Props) {
+export function Container(props: ContainerProps) {
   const theme = useContext(ThemeContext);
 
   return (
