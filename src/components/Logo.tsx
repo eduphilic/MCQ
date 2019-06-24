9986d3483b5390c39c6f4f81a30209e85473f8bb
strothj <jason@jasons.io>
2019-06-24 05:25:56 -0400

    refactor: extract logo image component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,19 +1,10 @@
 import { Typography } from "@material-ui/core";
 import React from "react";
 import styled, { css } from "styled-components";
-import logoImage48 from "./__assets__/logo48.png";
-import logoImage64 from "./__assets__/logo64.png";
+import { LogoImage, LogoImageProps } from "./LogoImage";
 
-type Props = {
+type Props = Pick<LogoImageProps, "size"> & {
   className?: string;
-
-  /**
-   * Logo image dimensions.
-   *
-   * @default 48
-   */
-  size?: 48 | 64;
-
   /**
    * Render logo text using only a single color rather than two.
    *
@@ -36,11 +27,6 @@ type Props = {
   hideTextMobile?: boolean;
 };
 
-const LogoImage = styled.img<Required<Pick<Props, "size">>>`
-  width: ${props => props.size}px;
-  height: ${props => props.size}px;
-`;
-
 const LogoText = styled(Typography)`
   font-size: 22px;
   font-weight: 600;
@@ -58,15 +44,11 @@ const hideTextMobileCss = css`
  * Site logo.
  */
 export const Logo = styled((props: Props) => {
-  const { size = 48, singleTone = false } = props;
+  const { size, singleTone = false } = props;
 
   return (
     <div className={props.className}>
-      <LogoImage
-        size={size}
-        src={size === 48 ? logoImage48 : logoImage64}
-        alt=""
-      />
+      <LogoImage size={size} />
       <LogoText component="span" color="primary">
         Join&nbsp;
       </LogoText>
---
+++ src/components/LogoImage.tsx
@@ -0,0 +1,29 @@
+import { ComponentPropsWithoutRef } from "react";
+import styled from "styled-components";
+import logoImage48 from "./__assets__/logo48.png";
+import logoImage64 from "./__assets__/logo64.png";
+
+type LogoImageSizeProp = {
+  /**
+   * Logo image dimensions.
+   *
+   * @default 48
+   */
+  size?: 48 | 64;
+};
+
+export type LogoImageProps = ComponentPropsWithoutRef<"img"> &
+  LogoImageSizeProp;
+
+/**
+ * Logo image.
+ */
+export const LogoImage = styled.img.attrs(
+  ({ size }: LogoImageSizeProp): LogoImageProps => ({
+    src: size === 48 ? logoImage48 : logoImage64,
+    alt: "",
+  }),
+)<LogoImageSizeProp>`
+  width: ${({ size }) => size}px;
+  height: ${({ size }) => size}px;
+`;
