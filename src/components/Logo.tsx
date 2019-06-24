567c537c064a4d111678abd931b8ca1731adf30d
strothj <jason@jasons.io>
2019-06-24 17:10:21 -0400

    refactor: extract logo text into component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,71 +1,28 @@
-import { Typography } from "@material-ui/core";
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { LogoImage, LogoImageProps } from "./LogoImage";
+import { LogoText, LogoTextProps } from "./LogoText";
 
-type Props = Pick<LogoImageProps, "size"> & {
-  className?: string;
-  /**
-   * Render logo text using only a single color rather than two.
-   *
-   * @default false
-   */
-  singleTone?: boolean;
-
-  /**
-   * Add optional text shadow.
-   *
-   * @default false
-   */
-  shadowed?: boolean;
-
-  /**
-   * Hide logo text on mobile.
-   *
-   * @default false
-   */
-  hideTextMobile?: boolean;
-};
-
-const LogoText = styled(Typography)`
-  font-size: 22px;
-  font-weight: 600;
-`;
-
-const hideTextMobileCss = css`
-  ${({ theme }) => theme.breakpoints.down("xs")} {
-    ${LogoText} {
-      display: none;
-    }
-  }
-`;
+type Props = Pick<LogoImageProps, "size"> & LogoTextProps;
 
 /**
  * Site logo.
  */
 export const Logo = styled((props: Props) => {
-  const { size, singleTone = false } = props;
+  const { className, size, ...rest } = props;
 
   return (
-    <div className={props.className}>
+    <div className={className}>
       <LogoImage size={size} />
-      <LogoText component="span" color="primary">
-        Join&nbsp;
-      </LogoText>
-      <LogoText component="span" color={singleTone ? "primary" : "secondary"}>
-        Uniform
-      </LogoText>
+      <LogoText {...rest} />
     </div>
   );
 })`
   display: flex;
   flex-shrink: 0;
   align-items: center;
-  ${({ shadowed }) => shadowed && "text-shadow: 1px 1px #000"};
 
   ${LogoImage} {
-    margin-right: ${({ theme }) => theme.spacing(2)}px;
+    margin-right: 16px;
   }
-
-  ${({ hideTextMobile }) => hideTextMobile && hideTextMobileCss};
 `;
---
+++ src/components/LogoText.tsx
@@ -0,0 +1,72 @@
+import { Typography } from "@material-ui/core";
+import React from "react";
+import styled, { css } from "styled-components";
+
+export type LogoTextProps = {
+  className?: string;
+
+  /**
+   * Render logo text using only a single color rather than two.
+   *
+   * @default false
+   */
+  singleTone?: boolean;
+
+  /**
+   * Render logo text using only a single color rather than two on mobile.
+   *
+   * @default false
+   */
+  singleToneMobile?: boolean;
+
+  /**
+   * Add optional text shadow.
+   *
+   * @default false
+   */
+  shadowed?: boolean;
+
+  /**
+   * Hide logo text on mobile.
+   *
+   * @default false
+   */
+  hideTextMobile?: boolean;
+};
+
+const singleToneCss = css`
+  span:nth-child(2) {
+    color: inherit;
+  }
+`;
+
+const singleToneMobileCss = css`
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    color: inherit;
+  }
+`;
+
+const hideTextMobileCss = css`
+  ${({ theme }) => theme.breakpoints.down("xs")} {
+    display: none;
+  }
+`;
+
+export const LogoText = styled(({ className }: LogoTextProps) => (
+  <Typography className={className} color="primary">
+    <span>Join&nbsp;</span>
+    <span>Uniform</span>
+  </Typography>
+))`
+  font-size: 22px;
+  font-weight: 600;
+
+  span:nth-child(2) {
+    color: ${({ theme }) => theme.palette.secondary.main};
+  }
+
+  ${({ singleTone }) => singleTone && singleToneCss};
+  ${({ singleToneMobile }) => singleToneMobile && singleToneMobileCss};
+  ${({ shadowed }) => shadowed && "text-shadow: 1px 1px #000"};
+  ${({ hideTextMobile }) => hideTextMobile && hideTextMobileCss};
+`;
