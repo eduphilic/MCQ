768bb96d2fcc795fec04d86b295bba87c178f28c
strothj <jason@jasons.io>
2019-07-11 11:20:37 -0400

    feat: add google signin/signup button

--- src/components/AuthenticationFormGoogleButton.tsx
+++ src/components/AuthenticationFormGoogleButton.tsx
@@ -1,62 +1,97 @@
 import React from "react";
-// import styled from "styled-components";
-// import robotoLatin500Woff from "typeface-roboto/files/roboto-latin-500.woff";
-// import robotoLatin500Woff2 from "typeface-roboto/files/roboto-latin-500.woff2";
-// import { ReactComponent as GoogleIconDisabled } from "./__assets__/signin-assets/google_signin_buttons/web/vector/btn_google_dark_disabled_ios.svg";
-// import { ReactComponent as GoogleIconFocus } from "./__assets__/signin-assets/google_signin_buttons/web/vector/btn_google_dark_focus_ios.svg";
-// import { ReactComponent as GoogleIconNormal } from "./__assets__/signin-assets/google_signin_buttons/web/vector/btn_google_dark_normal_ios.svg";
-// import { ReactComponent as GoogleIconPressed } from "./__assets__/signin-assets/google_signin_buttons/web/vector/btn_google_dark_pressed_ios.svg";
+import styled from "styled-components";
+import robotoLatin500Woff from "typeface-roboto/files/roboto-latin-500.woff";
+import robotoLatin500Woff2 from "typeface-roboto/files/roboto-latin-500.woff2";
+import { AuthenticationFormGoogleButtonIcon } from "./AuthenticationFormGoogleButtonIcon";
+import Button, { ButtonProps } from "@material-ui/core/Button";
 
-export function AuthenticationFormGoogleButton() {
+interface Props
+	extends OmitStrict<ButtonProps, "children" | "variant" | "fullWidth"> {
+	children: string;
+}
+
+/**
+ * Google signin/signup button which attempts to adhere to Google's branding
+ * standards.
+ *
+ * @see https://developers.google.com/identity/branding-guidelines
+ */
+export function AuthenticationFormGoogleButton(props: Props) {
+	const { children, disabled, ...rest } = props;
+
 	return (
-		// <StyledButton>
-		//   {/* <StyledGoogleIconNormal /> */}
-		//   <StyledSpan>Sign up with Google</StyledSpan>
-		// </StyledButton>
-		<>{null}</>
+		<StyledButton
+			variant="contained"
+			fullWidth
+			disabled={disabled}
+			{...rest}
+		>
+			{/* Pass along the "disabled" prop to render alternate SVG. */}
+			<StyledAuthenticationFormGoogleButtonIcon disabled={disabled} />
+			<StyledSpan>{children}</StyledSpan>
+		</StyledButton>
 	);
 }
 
-// const StyledGoogleIconDisabled = styled(GoogleIconDisabled)``;
-// const StyledGoogleIconFocus = styled(GoogleIconFocus)``;
-// const StyledGoogleIconNormal = styled(GoogleIconNormal)``;
-// const StyledGoogleIconPressed = styled(GoogleIconPressed)``;
+const iconWidth = 38;
 
-// const StyledButton = styled.button`
-//   @font-face {
-//     font-family: 'Roboto';
-//     font-style: normal;
-//     font-display: swap;
-//     font-weight: 500;
-//     src:
-//       local('Roboto Medium '),
-//       local('Roboto-Medium'),
-//       url('${robotoLatin500Woff2}') format('woff2'),
-//       url('${robotoLatin500Woff}') format('woff');
-//   }
+const StyledAuthenticationFormGoogleButtonIcon = styled(
+	AuthenticationFormGoogleButtonIcon,
+)`
+	width: ${iconWidth}px;
+	height: ${iconWidth}px;
+	border-top-left-radius: 4px;
+	border-bottom-left-radius: 4px;
 
-//   display: inline-flex;
-//   align-items: center;
-//   padding: 0;
-//   width: 100%;
-//   border: none;
-//   color: #fff;
-//   background-color: #4285f4;
-//   cursor: pointer;
+	/* Zoom in the Google logo ("G") and adjust its position. */
+	g {
+		transform: scale(1.2) translate(-7%, -7%);
+	}
+`;
 
-// `;
+const StyledSpan = styled.span`
+	flex: 1;
+	padding-right: ${iconWidth / 2}px;
+	font-family: "Roboto", sans-serif;
+	font-size: 14px;
+`;
 
-// const StyledSpan = styled.span`
-//   font-family: "Roboto", sans-serif;
-//   font-size: 14px;
-// `;
+const StyledButton = styled(Button)`
+	@font-face {
+		font-family: 'Roboto';
+		font-style: normal;
+		font-display: swap;
+		font-weight: 500;
+		src:
+			local('Roboto Medium '),
+			local('Roboto-Medium'),
+			url('${robotoLatin500Woff2}') format('woff2'),
+			url('${robotoLatin500Woff}') format('woff');
+	}
 
-/* ${StyledGoogleIconDisabled},
-  ${StyledGoogleIconFocus},
-  ${StyledGoogleIconNormal},
-  ${StyledGoogleIconPressed} {
-    width: 36px;
-    height: 36px;
-    margin-right: 16px;
-    transform: scale(1.14);
-  } */
+	justify-content: space-between;
+	padding: 1px;
+	color: #fff;
+	background-color: #4285f4;
+	cursor: pointer;
+
+	${StyledAuthenticationFormGoogleButtonIcon} {
+		background-color: #fff;
+	}
+
+	&.Mui-disabled ${StyledAuthenticationFormGoogleButtonIcon} {
+		background-color: transparent;
+	}
+
+	&.Mui-disabled ${StyledAuthenticationFormGoogleButtonIcon} path {
+		fill: ${({ theme }) => theme.palette.action.disabled};
+	}
+
+	/* Reset on touch devices, it doesn't add specificity. */
+	:hover {
+		background-color: #3367d6;
+	}
+	@media (hover: none) {
+		background-color: #4285f4;
+	}
+`;
